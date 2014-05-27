208848455393e11d74458e42eacd68a15d32e12e
eridai <eric_dai@live.com>
2014-05-27 13:53:53 +0800

    Fix multiple enabling issue(enable when reboot)

--- CustomScript/installer/handle.py
+++ CustomScript/installer/handle.py
@@ -56,19 +56,31 @@ def main():
         elif re.match("^([-/]*)(install)", a):
             install()
         elif re.match("^([-/]*)(enable)", a):
-            install()
-
+            enable()
+        elif re.match("^([-/]*)(update)", a):
+            update()
 def install():
+    name,seqNo,version,config_dir,log_dir,settings_file,status_file,heartbeat_file,config=hutil.doParse(logfile,'Uninstall')
+    hutil.doExit(name,seqNo,version,0,status_file,heartbeat_file,'Install','Installed','0', 'Install Succeeded', 'NotReady','0',name+' Installed.')
+
+def enable():
     try:
         name,seqNo,version,config_dir,log_dir,settings_file,status_file,heartbeat_file,config=hutil.doParse(logfile,'Install')
         LoggerInit('/var/log/'+name+'_Install.log','/dev/stdout')
-        waagent.Log(name+" - install starting.")
+        # Ensure the same configuration is executed only once
+        # If the previous enable failed, we do not have retry logic here. Since the custom script may not work in an intermediate state
+        if(int(seqNo) <= get_most_recent_seq()):
+            waagent.Log("Current sequence number, " + seqNo + ", is not greater than the sequnce number of the most recent executed configuration. Exiting...")
+            sys.exit(0)
+        set_most_recent_seq(seqNo)
+        waagent.Log("set most recent sequence number to , " + seqNo + ", done. Start enabling.")        
         protected_settings = config['runtimeSettings'][0]['handlerSettings']['protectedSettings']
         public_settings = config['runtimeSettings'][0]['handlerSettings']['publicSettings']
         #get script in storage blob
         storage_account_name = protected_settings["storageAccountName"]
         storage_account_key = protected_settings["storageAccountKey"]
         blob_uris = public_settings['fileUris']
+        hutil.doStatusReport(name,seqNo,version,status_file,'NotReady', '0', 'Downloading files...')
         for blob_uri in blob_uris:
             download_blob(storage_account_name, storage_account_key, blob_uri, seqNo)
         #execute the script
@@ -76,6 +88,7 @@ def install():
         cmd = re.sub('\s+',' ', cmd).strip()
         waagent.Log("Command to execute:" + cmd)
         args = cmd.split(' ')
+        hutil.doStatusReport(name,seqNo,version,status_file,'NotReady', '0', 'Executing commands...')
         if len(blob_uris) > 0:
             download_dir = get_download_directory(seqNo)
             os.chdir(download_dir)
@@ -84,21 +97,33 @@ def install():
             subprocess.Popen(args)
         #report the status
         hutil.doExit(name,seqNo,version,0,status_file,
-                heartbeat_file,'Install','Ready','0', 'Install Succeeded.',
-                'Ready','0',name+' installation completed.')
+                heartbeat_file,'Enable','Ready','0', 'Enable Succeeded.',
+                'Ready','0',name+' Enable completed.')
     except Exception, e:
-        waagent.Log("Failed to install/enable the extension with error:" + str(e))
+        waagent.Log("Failed to enable the extension with error:" + str(e))
         hutil.doExit(name,seqNo,version,1,status_file,
-                heartbeat_file,'Install','Not Ready','1', 'Install failed.',
-                'Not Ready','1',name+' installation failed.')
+                heartbeat_file,'Enable','Error','0', 'Enable failed.',
+                'NotReady','1',name+' enable failed.')
 def uninstall():
     name,seqNo,version,config_dir,log_dir,settings_file,status_file,heartbeat_file,config=hutil.doParse(logfile,'Uninstall')
-    hutil.doExit(name,seqNo,version,0,status_file,heartbeat_file,'Uninstall','Uninstalled','0', 'Uninstall Succeeded', 'Ready','0',name+' uninstalled.')
+    hutil.doExit(name,seqNo,version,0,status_file,heartbeat_file,'Uninstall','Uninstalled','0', 'Uninstall Succeeded', 'NotReady','0',name+' uninstalled.')
 
 def disable():
     name,seqNo,version,config_dir,log_dir,settings_file,status_file,heartbeat_file,config=hutil.doParse(logfile,'Disable')
-    hutil.doExit(name,seqNo,version,0,status_file,heartbeat_file,'Disable','success','0', 'Disable service.py Succeeded', 'Ready','0',name+' disabled.')
+    hutil.doExit(name,seqNo,version,0,status_file,heartbeat_file,'Disable','success','0', 'Disable service.py Succeeded', 'NotReady','0',name+' disabled.')
 
+def update():
+    name,seqNo,version,config_dir,log_dir,settings_file,status_file,heartbeat_file,config=hutil.doParse(logfile,'Update')
+    hutil.doExit(name,seqNo,version,0,status_file,heartbeat_file,'Update','transitioning','0', 'Updating', 'NotReady','0',name+' updating.')
+
+def get_most_recent_seq():
+    seq = waagent.GetFileContents('mrseq')
+    if(seq):
+        return int(seq)
+    return 0
+
+def set_most_recent_seq(seq):
+    waagent.SetFileContents('mrseq', str(seq))
 
 def get_blob_name_from_uri(uri):
     return get_properties_from_uri(uri)['blob_name']
@@ -119,7 +144,7 @@ def download_blob(storage_account_name, storage_account_key, blob_uri, seqNo):
     blob_name = get_blob_name_from_uri(blob_uri)
     download_dir = get_download_directory(seqNo)
     download_path = os.path.join(download_dir, blob_name)
-    #TODO:consider the scenario the blob already exists, think of swap the existing and the new downloaded one
+    # Guest agent already ensure the plugin is enabled one after another. The blob download will not conflict.
     blob_service = BlobService(storage_account_name, storage_account_key)
     try:
         blob_service.get_blob_to_path(container_name, blob_name, download_path)
