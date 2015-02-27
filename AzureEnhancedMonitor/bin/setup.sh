f6da497c50e4a739c525871e52a43f995eabbb4a
Yue Zhang <yuezha@microsoft.com>
2015-02-27 17:25:34 +0800

    Add support for SUSE

--- AzureEnhancedMonitor/bin/setup.sh
+++ AzureEnhancedMonitor/bin/setup.sh
@@ -10,6 +10,43 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+function install_nodejs_suse()
+{
+    suse_version="SLE_11_SP3"
+    if [ "$(grep 'openSUSE 13.1' /etc/*release*)" != "" ] ; then
+        suse_version="openSUSE_13.1"
+    elif [ "$(grep 'openSUSE 13.2' /etc/*release*)" != "" ] ; then
+        suse_version="openSUSE_13.2"
+    elif [ "$(grep 'SUSE Linux Enterprise Server 11' /etc/*release*)" != "" ] ; then
+        if [ "$(grep 'PATCHLEVEL = 3' /etc/*release*)" != "" ] ; then
+            suse_version="SLE_11_SP3"
+        elif [ "$(grep 'PATCHLEVEL = 2' /etc/*release*)" != "" ] ; then
+            suse_version="SLE_11_SP2"
+        elif [ "$(grep 'PATCHLEVEL = 1' /etc/*release*)" != "" ] ; then
+            suse_version="SLE_11_SP1"
+        else
+            suse_version="SLE_11"
+        fi
+    elif [ "$(grep 'SUSE Linux Enterprise Server 12' /etc/*release*)" != "" ] ; then
+        suse_version="SLE_12"
+    fi
+    
+    set -e
+    if [ "$(zypper repos | grep devel_languages_nodejs)" != "" ] ; then 
+        zypper removerepo devel_languages_nodejs 1>>$install_log 2>&1
+    fi
+
+    zypper addrepo http://download.opensuse.org/repositories/devel:languages:nodejs/$suse_version/devel:languages:nodejs.repo 1>>$install_log 2>&1
+    zypper --gpg-auto-import-keys refresh 1>>$install_log 2>&1
+
+    #Install nodejs
+    zypper install -yl nodejs 1>>$install_log 2>&1
+
+    #Install npm
+    curl -sL https://www.npmjs.org/install.sh | sh 1>>$install_log 2>&1
+    set +e
+}
+
 function install_nodejs()
 {
     echo "[INFO]Installing nodejs and npm"
@@ -19,11 +56,14 @@ function install_nodejs()
     elif [ "$(type yum 2>/dev/null)" != "" ] ; then
         curl -sL https://rpm.nodesource.com/setup | bash - 1>>$install_log 2>&1
         yum -y install nodejs 1>>$install_log 2>&1
+    elif [ "$(type zypper 2>/dev/null)" != "" ] ; then
+        install_nodejs_suse
     else
-        echo "[ERROR]Neither apt-get or yum is found, you need to install nodejs manually."
+        echo "[ERROR]Neither apt-get, yum or zypper is found, you need to install nodejs manually."
         echo ""
         echo "    You could refer to https://github.com/joyent/node/wiki/installing-node.js-via-package-manager."
         echo ""
+        echo "[ERROR]Install nodejs and npm failed. See $install_log."
         exit 1
     fi
     if [ ! $? ]; then
