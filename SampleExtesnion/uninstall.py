03c447e85bb7cc0bc1b3a72864f9c5b1df9cae8e
Bin Xia <binxi@microsoft.com>
2015-11-13 07:02:04 +0000

    Add a sample extension

---
+++ SampleExtesnion/uninstall.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from Utils.WAAgentUtil import waagent
+import Utils.HandlerUtil as Util
+
+ExtensionShortName = "SampleExtension"
+
+def main():
+    #Global Variables definition
+    waagent.LoggerInit('/var/log/waagent.log','/dev/stdout')
+    waagent.Log("%s started to handle." %(ExtensionShortName))
+
+    operation = "uninstall"
+    status = "success"
+    msg = "Uninstalled successfully."
+
+    hutil = parse_context(operation)
+    hutil.log("Start to uninstall.")
+    hutil.log(msg)
+    hutil.do_exit(0, operation, status, '0', msg)
+
+
+def parse_context(operation):
+    hutil = Util.HandlerUtility(waagent.Log, waagent.Error, ExtensionShortName)
+    hutil.do_parse_context(operation)
+    return hutil
+
+
+if __name__ == '__main__' :
+    main()
