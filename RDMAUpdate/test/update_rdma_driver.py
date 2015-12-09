d43578adad925708d513e69c9306bfeed792a057
andyliuliming <andyliuliming@outlook.com>
2015-12-09 12:48:54 +0800

    update rdma driver test update.
    update rdma driver test update.

--- RDMAUpdate/test/update_rdma_driver.py
+++ RDMAUpdate/test/update_rdma_driver.py
@@ -1,23 +1,36 @@
-﻿import subprocess, re
+import subprocess, re, os
 
 def RunGetOutput(cmd,chk_err=True):
-    try:
-        output=subprocess.check_output(cmd,stderr=subprocess.STDOUT,shell=True)
-    except subprocess.CalledProcessError,e :
-        if chk_err :
-            print('CalledProcessError.  Error Code is ' + str(e.returncode)  )
-            print('CalledProcessError.  Command string was ' + e.cmd  )
-            print('CalledProcessError.  Command result was ' + (e.output[:-1]).decode('latin-1'))
-        return e.returncode,e.output.decode('latin-1')
-    return 0,output.decode('latin-1')
+	try:
+		output=subprocess.check_output(cmd,stderr=subprocess.STDOUT,shell=True)
+	except subprocess.CalledProcessError,e :
+		if chk_err :
+			print('CalledProcessError.  Error Code is ' + str(e.returncode)  )
+			print('CalledProcessError.  Command string was ' + e.cmd  )
+			print('CalledProcessError.  Command result was ' + (e.output[:-1]).decode('latin-1'))
+		return e.returncode,e.output.decode('latin-1')
+	return 0,output.decode('latin-1')
+#def
 
+def InstallRDMADriver(host_version) :
+	RunGetOutput("zypper --non-interactive install msft-rdma-drivers")
+	r = os.listdir("/opt/microsoft/rdma")
+	if r :
+		for filename in r :
+			if re.match("msft-lis-rdma-kmp-default-\d{8}\.(%s).+" % host_version, filename) :
+				print "Installing RPM /opt/microsoft/rdma/" + filename
+				RunGetOutput("zypper --non-interactive install /opt/microsoft/rdma/%s" % filename)
+				return
+
+	print "RDMA drivers not found in /opt/microsoft/rdma"
+#def
 
 #1. check if kvp daemon is running, if not install it and reboot
 error, output = RunGetOutput("ps -ef")	# how about error != 0
 r = re.search("hv_kvp_daemon", output)
 if not r :
 	print "KVP deamon is not running, install it"
-	RunGetOutput("zypper install --no-confirm hyper-v")	#find a way to force install non-prompt
+	RunGetOutput("zypper --non-interactive install hyper-v")	#find a way to force install non-prompt
 	RunGetOutput("reboot")
 else :
 	print "KVP deamon is running"
@@ -38,18 +51,18 @@ else :
 
 
 #3. if the ND version doesn't match the RDMA driver package version, do an update
-error, output = RunGetOutput("zypper info msft-lis-rdma-kmp-default")
+error, output = RunGetOutput("zypper --non-interactive info msft-lis-rdma-kmp-default")
 
-r = re.search("Version: (\S+)", output)
+r = re.search("Version:\s+(\S+)", output)
 if r :
-	package_version = r.groups()[0]		# e.g. package_version is "20150707_k3.12.28_4-3.1.140.0"
-        print "msft-lis-rdma-kmp-default package version = " + package_version
+	package_version = r.groups()[0]		# e.g. package_version is "20151119.142.0_k3.12.28_4-1.1"
+	print "msft-lis-rdma-kmp-default package version = " + package_version
 
-	NdDriverVersion = "4-3.1"
-	r = re.match(".+(%s)$" % NdDriverVersion, package_version)	# NdDriverVersion should be at the end of package version
+	r = re.match("\d{8}\.(%s).+" % NdDriverVersion, package_version)	# NdDriverVersion should be at the end of package version
 	if not r :	#host ND version is the same as the package version, do an update
 		print "ND and package version don't match, doing an update"
-		RunGetOutput("zypper dup --no-confirm");	#this will update everything, need to find a way to update only the RDMA driver
+		RunGetOutput("zypper --non-interactive remove msft-lis-rdma-kmp-default")
+		InstallRDMADriver(NdDriverVersion)
 		RunGetOutput("reboot")
 	else :
 		print "ND and package version match, not doing an update"
@@ -57,5 +70,5 @@ if r :
 else :
 	print "msft-lis-rdma-kmp-default not found, installing new version"
 
-	RunGetOutput("zypper install --no-confirm msft-lis-rdma-kmp-default-20151119_k3.12.28_4-14.1");
+	InstallRDMADriver(NdDriverVersion)
 	RunGetOutput("reboot");
