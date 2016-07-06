01bda2556f416eb813844663114db2f8042997c9
Kamran Khan <kakhan@microsoft.com>
2016-07-05 19:26:19 -0700

    Revert "Use umount() method from BekUtil to perform the unmount"
    This reverts commit 39f20d2f13ed9450fecd6f588bb62ceb12a431b7.

--- VMEncryption/main/BekUtil.py
+++ VMEncryption/main/BekUtil.py
@@ -69,8 +69,7 @@ class BekUtil(object):
 
         return None
 
-    def umount_azure_passhprase(self, encryption_config, force=False):
+    def umount_azure_passhprase(self, encryption_config):
         passphrase_file = self.get_bek_passphrase_file(encryption_config)
-        if not os.path.exists(passphrase_file) and not force:
-            return
-        self.disk_util.umount(self.bek_filesystem_mount_point)
+        if os.path.exists(passphrase_file):
+                    self.disk_util.umount(self.bek_filesystem_mount_point)
