cf1a01fb1eed59f5b764ebd124d029be0b9efc08
Kamran Khan <kakhan@microsoft.com>
2016-08-31 14:14:14 -0700

    Add max retry attempts for unmount loops

--- VMEncryption/main/oscrypto/encrypt.py
+++ VMEncryption/main/oscrypto/encrypt.py
@@ -180,6 +180,8 @@ class OSEncryption(object):
             try:
                 if attempt == 1:
                     self.enter_unmount_oldroot()
+                elif attempt > 10:
+                    raise Exception("Could not unmount /oldroot in 10 attempts")
                 else:
                     self.retry_unmount_oldroot()
 
