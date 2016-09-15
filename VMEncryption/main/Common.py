56175548c990bb7a98a59cc5d5441e247b4e778c
Kamran Khan <kakhan@microsoft.com>
2016-09-14 18:37:05 -0700

    Allow old size of LUKS headers (2056 sectors)

--- VMEncryption/main/Common.py
+++ VMEncryption/main/Common.py
@@ -30,6 +30,7 @@ class CommonVariables:
     disk/file system related
     """
     sector_size = 512
+    old_luks_header_size = 2056 * 512
     luks_header_size = 4096 * 512
     default_block_size = 52428800
     min_filesystem_size_support = 52428800 * 3
