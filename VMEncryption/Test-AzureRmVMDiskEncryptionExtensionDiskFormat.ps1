cd7d08dd72934f8d5ea65971f24471d2801b2fbf
Kamran Khan <kakhan@microsoft.com>
2016-09-17 23:44:31 -0700

    Create RAID array in a separate call

--- VMEncryption/Test-AzureRmVMDiskEncryptionExtensionDiskFormat.ps1
+++ VMEncryption/Test-AzureRmVMDiskEncryptionExtensionDiskFormat.ps1
@@ -180,6 +180,17 @@ exit
     $commands = @"
 apt-get install -yq mdadm
 yum install -y mdadm
+exit
+"@
+
+    $commands | Out-File -Encoding ascii $commandFileName
+    dos2unix $commandFileName
+    cmd /c "ssh -o UserKnownHostsFile=C:\Windows\System32\NUL -o StrictHostKeyChecking=no -i $SshPrivKeyPath root@${Hostname} <$commandFileName"
+    Remove-Item $commandFileName
+
+    Write-Host "Installed mdadm"
+
+    $commands = @"
 mdadm --create --verbose /dev/md0 --level=0 --raid-devices=2 /dev/sdc /dev/sdd
 mkdir -p /etc/mdadm
 mdadm --detail --scan > /etc/mdadm/mdadm.conf
