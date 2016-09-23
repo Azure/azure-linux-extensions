b0a9d7f277b0c3d2123194067801ab83a178894b
Kamran Khan <kakhan@microsoft.com>
2016-09-23 01:20:47 -0700

    Update storage account

--- VMBackup/main/common.py
+++ VMBackup/main/common.py
@@ -22,9 +22,9 @@ class CommonVariables:
     azure_path = 'main/azure'
     utils_path_name = 'Utils'
     extension_name = 'VMBackupForLinuxExtension'
-    extension_version = "0.1.0.994"
+    extension_version = "0.1.0.995"
     extension_type = extension_name
-    extension_media_link = 'https://andliu.blob.core.windows.net/extensions/' + extension_name + '-' + str(extension_version) + '.zip'
+    extension_media_link = 'https://amextpaas.blob.core.windows.net/prod/' + extension_name + '-' + str(extension_version) + '.zip'
     extension_label = 'Windows Azure VMBackup Extension for Linux IaaS'
     extension_description = extension_label
     object_str = 'objectStr'
