82eb32f7456b7496cab8517610932f2b925a7535
Kamran Khan <kakhan@microsoft.com>
2016-11-19 17:16:25 -0800

    Revert "Fix systemctl call"
    This reverts commit dcda0921e90e3e0f549f525c311ffc6d31a13425.

--- VMEncryption/main/oscrypto/rhel_72/encryptstates/PatchBootSystemState.py
+++ VMEncryption/main/oscrypto/rhel_72/encryptstates/PatchBootSystemState.py
@@ -75,7 +75,7 @@ class PatchBootSystemState(OSEncryptionState):
                                           True)
             self.command_executor.Execute('umount /boot')
             self.command_executor.Execute('umount /oldroot')
-            self.command_executor.Execute('service waagent restart')
+            self.command_executor.Execute('systemctl restart waagent')
 
             self.context.logger.log("Pivoted back into memroot successfully")
 
