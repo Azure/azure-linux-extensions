8b3a6e5801b90348235a76e1b4b43d19da0f9dfa
vityagi <vityagi@microsoft.com>
2016-12-02 07:43:11 -0500

    managed disk pageblob changes 1

--- VMBackup/main/blobwriter.py
+++ VMBackup/main/blobwriter.py
@@ -24,6 +24,13 @@ import httplib
 from common import CommonVariables
 from HttpUtil import HttpUtil
 
+class BlobProperties():
+    def __init__(self, blobType, contentLength):
+        self.blobType = blobType
+        self.contentLength = contentLength
+    def __str__(self):
+        return ' blobType: ' + str(self.blobType) + ' contentLength: ' + str(self.contentLength)
+
 class BlobWriter(object):
     """description of class"""
     def __init__(self, hutil):
@@ -32,6 +39,23 @@ class BlobWriter(object):
     network call should have retry.
     """
     def WriteBlob(self,msg,blobUri):
+        try:
+            # get the blob type
+            if(blobUri is not None):
+                blobType = self.GetBlobType(blobUri)
+                if (str(blobType).lower() == "pageblob"):
+                    # Clear Page-Blob Contents
+                    self.ClearPageBlob(blobUri)
+                    # Write to Page-Blob
+                    self.WritePageBlob(msg, blobUri)
+                else:
+                    self.WriteBlockBlob(msg, blobUri)
+            else:
+                self.hutil.log("bloburi is None")
+        except Exception as e:
+            self.hutil.log("Failed to committing the log with error: %s, stack trace: %s" % (str(e), traceback.format_exc()))
+
+    def WriteBlockBlob(self,msg,blobUri):
         retry_times = 3
         while(retry_times > 0):
             try:
@@ -49,9 +73,152 @@ class BlobWriter(object):
                     else:
                         self.hutil.log("blob failed to write")
                 else:
-                    self.hutil.log("logbloburi is None")
+                    self.hutil.log("bloburi is None")
                     retry_times = 0
             except Exception as e:
                 self.hutil.log("Failed to committing the log with error: %s, stack trace: %s" % (str(e), traceback.format_exc()))
             self.hutil.log("retry times is " + str(retry_times))
             retry_times = retry_times - 1
+
+    def WritePageBlob(self, msg, blobUri):
+        if(blobUri is not None):
+            retry_times = 3
+            while(retry_times > 0):
+                try:
+                    PAGE_UPLOAD_LIMIT_BYTES = 10240 # 2097152 # 4194304
+                    http_util = HttpUtil(self.hutil)
+                    sasuri_obj = urlparse.urlparse(blobUri + '&comp=page')
+                    # Add padding to message to make its legth multiple of 512
+                    loopIndex = 0
+                    while(loopIndex < 6):
+                        msg = msg + msg
+                        loopIndex = loopIndex + 1
+                    msgLen = len(msg)
+                    self.hutil.log("msgLen:"+str(msgLen))
+                    remainder = msgLen % 512
+                    if(remainder != 0):
+                        msgLen = msgLen + (512 - remainder)
+                    msg = msg.ljust(msgLen)
+                    self.hutil.log("msgLen:"+str(len(msg)))
+                    # Write Pages
+                    result = CommonVariables.error
+                    index = 0
+                    bytes_sent = 0
+                    while (bytes_sent < msgLen):
+                        bytes_remaining = msgLen - bytes_sent
+                        pageContent = None
+                        if(bytes_remaining > PAGE_UPLOAD_LIMIT_BYTES): # more than 4 MB
+                            pageContent = msg[index:index+PAGE_UPLOAD_LIMIT_BYTES]
+                        else:
+                            pageContent = msg[index:msgLen]
+                        self.hutil.log("pageContentLen:"+str(len(pageContent)))
+                        self.hutil.log("pageContent:"+pageContent)
+                        result = self.put_page_update(pageContent, blobUri, bytes_sent)
+                        if(result == CommonVariables.success):
+                            self.hutil.log("page written succesfully")
+                        else:
+                            self.hutil.log("page failed to write")
+                            break
+                        bytes_sent = bytes_sent + len(pageContent)                      
+                    if(result == CommonVariables.success):
+                        self.hutil.log("page-blob written succesfully")
+                        retry_times = 0
+                    else:
+                        self.hutil.log("page-blob failed to write")
+                except Exception as e:
+                    self.hutil.log("Failed to write to page-blob with error: %s, stack trace: %s" % (str(e), traceback.format_exc()))
+                self.hutil.log("retry times is " + str(retry_times))
+                retry_times = retry_times - 1
+        else:
+            self.hutil.log("bloburi is None")
+
+    def ClearPageBlob(self, blobUri):
+        if(blobUri is not None):
+            retry_times = 3
+            while(retry_times > 0):
+                try:
+                    http_util = HttpUtil(self.hutil)
+                    sasuri_obj = urlparse.urlparse(blobUri + '&comp=page')
+                    # Get Blob-properties to know content-length
+                    blobProperties = self.GetBlobProperties(blobUri)
+                    contentLength = int(blobProperties.contentLength)
+                    self.hutil.log("ClearPageBlob contentLength:"+str(contentLength))
+                    # Clear Pages
+                    if(contentLength > 0):
+                        result = self.put_page_clear(blobUri, 0, contentLength)
+                        if(result == CommonVariables.success):
+                            self.hutil.log("page-blob cleared succesfully")
+                            retry_times = 0
+                        else:
+                            self.hutil.log("page-blob failed to clear")
+                    else:
+                        retry_times = 0
+                except Exception as e:
+                    self.hutil.log("Failed to clear to page-blob with error: %s, stack trace: %s" % (str(e), traceback.format_exc()))
+                self.hutil.log("retry times is " + str(retry_times))
+                retry_times = retry_times - 1
+        else:
+            self.hutil.log("bloburi is None")
+
+    def GetBlobType(self, blobUri):
+        blobType = "BlockBlob"
+        if(blobUri is not None):
+            # Get Blob Properties
+            blobProperties = self.GetBlobProperties(blobUri)
+            if(blobProperties is not None):
+                blobType = blobProperties.blobType
+        self.hutil.log("Blob-Type :"+str(blobType))
+        return blobType
+
+    def GetBlobProperties(self, blobUri):
+        blobProperties = None
+        if(blobUri is not None):
+            retry_times = 3
+            while(retry_times > 0):
+                try:
+                    http_util = HttpUtil(self.hutil)
+                    sasuri_obj = urlparse.urlparse(blobUri)
+                    headers = {}
+                    httpResp = http_util.HttpCallGetResponse('GET', sasuri_obj, None, headers = headers)
+                    blobProperties = self.httpresponse_get_blob_properties(httpResp)
+                    retry_times = 0
+                except Exception as e:
+                    self.hutil.log("Failed to get blob properties with error: %s, stack trace: %s" % (str(e), traceback.format_exc()))
+                    self.hutil.log("retry times is " + str(retry_times))
+                    retry_times = retry_times - 1
+        return blobProperties
+
+    def put_page_clear(self, blobUri, pageBlobIndex, clearLength):
+         http_util = HttpUtil(self.hutil)
+         sasuri_obj = urlparse.urlparse(blobUri + '&comp=page')
+         headers = {}
+         headers["x-ms-page-write"] = 'clear'
+         headers["x-ms-range"] = 'bytes={}-{}'.format(pageBlobIndex, pageBlobIndex + clearLength - 1)
+         headers["Content-Length"] = 0
+         self.hutil.log(str(headers))
+         result = http_util.Call(method = 'PUT', sasuri_obj = sasuri_obj, data = None, headers = headers, fallback_to_curl = False)
+         return result
+
+    def put_page_update(self, pageContent, blobUri, pageBlobIndex):
+         http_util = HttpUtil(self.hutil)
+         sasuri_obj = urlparse.urlparse(blobUri + '&comp=page')
+         headers = {}
+         headers["x-ms-page-write"] = 'update'
+         headers["x-ms-range"] = 'bytes={}-{}'.format(pageBlobIndex, pageBlobIndex + len(pageContent) - 1)
+         headers["Content-Length"] = len(str(pageContent))
+         self.hutil.log(str(headers))
+         result = http_util.Call(method = 'PUT', sasuri_obj = sasuri_obj, data = pageContent, headers = headers, fallback_to_curl = False)
+         return result
+
+    def httpresponse_get_blob_properties(self, httpResp):
+        blobProperties = None
+        if(httpResp != None):
+            self.hutil.log("Blob-properties response status:"+str(httpResp.status))
+            if(httpResp.status == 200):
+                resp_headers = httpResp.getheaders()
+                self.hutil.log("Blob-properties resp_headers:"+str(resp_headers))
+                blobType = httpResp.getheader('x-ms-blob-type')
+                contentLength = httpResp.getheader('Content-Length')
+                blobProperties = BlobProperties(blobType, contentLength)
+        return blobProperties
+
