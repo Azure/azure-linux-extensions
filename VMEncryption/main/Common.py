fc255dba73c716f8d88a9530bbb4b227bed5d2ef
Kamran Khan <kakhan@microsoft.com>
2016-12-20 17:32:13 -0800

    Bump version number

--- VMEncryption/main/Common.py
+++ VMEncryption/main/Common.py
@@ -20,7 +20,7 @@
 class CommonVariables:
     utils_path_name = 'Utils'
     extension_name = 'AzureDiskEncryptionForLinuxTest'
-    extension_version = '0.1.0.999234'
+    extension_version = '0.1.0.999235'
     extension_type = extension_name
     extension_media_link = 'https://amextpaas.blob.core.windows.net/prod/' + extension_name + '-' + str(extension_version) + '.zip'
     extension_label = 'Windows Azure VMEncryption Extension for Linux IaaS'
