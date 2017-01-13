b8a02de98c1688ff91c6f82b3f3b2ffcac59f4d9
Kamran Khan <kakhan@microsoft.com>
2017-01-13 14:42:01 -0800

    Fix exit condition for EncryptBlockDevice

--- VMEncryption/main/oscrypto/rhel_72_lvm/encryptstates/EncryptBlockDeviceState.py
+++ VMEncryption/main/oscrypto/rhel_72_lvm/encryptstates/EncryptBlockDeviceState.py
@@ -64,9 +64,6 @@ class EncryptBlockDeviceState(OSEncryptionState):
         if not os.path.exists('/dev/mapper/osencrypt'):
             self._find_bek_and_execute_action('_luks_open')
 
-        self.command_executor.Execute('mount /dev/mapper/osencrypt /oldroot', True)
-        self.command_executor.Execute('umount /oldroot', True)
-
         return super(EncryptBlockDeviceState, self).should_exit()
 
     def _luks_format(self, bek_path):
