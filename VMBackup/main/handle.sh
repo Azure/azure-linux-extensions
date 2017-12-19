8b5f54c232b28d0b5ae67d946123e32649451d1f
soumyanishan <sopattna@microsoft.com>
2017-12-19 13:47:05 +0000

    code review changes

--- VMBackup/main/handle.sh
+++ VMBackup/main/handle.sh
@@ -10,63 +10,55 @@ resultstrlen=`expr ${#postsubstr} - 1 - ${#postsubstr1}`
 logfolder=$(echo $postsubstr | cut -b 1-$resultstrlen)
 logfile=$logfolder'/shell.log'
 
+rc=3
 if [ -f "/usr/bin/python2.7" ]
 then
     echo "`date`- python 2.7 path exists" >> $logfile
     /usr/bin/python2.7 main/handle.py -$1
     rc=$?
-    echo "`date`- $rc returned from handle.py" >> $logfile
 elif [ -f "/usr/bin/python2" ]
 then
     echo "`date`- python2 path exists" >> $logfile
     /usr/bin/python2 main/handle.py -$1
     rc=$?
-    echo "`date`- $rc returned from handle.py" >> $logfile
 elif [ -f "/usr/bin/python3.6" ]
 then
     echo "`date`- python3.6 path exists" >> $logfile
     /usr/bin/python3.6 main/handle.py -$1
     rc=$?
-    echo "`date`- $rc returned from handle.py" >> $logfile
 elif [ -f "/usr/bin/python3.5" ]
 then
     echo "`date`- python3.5 path exists" >> $logfile
     /usr/bin/python3.5 main/handle.py -$1
     rc=$?
-    echo "`date`- $rc returned from handle.py" >> $logfile
 elif [ -f "/usr/bin/python3.4" ]
 then
     echo "`date`- python3.4 path exists" >> $logfile
     /usr/bin/python3.4 main/handle.py -$1
     rc=$?
-    echo "`date`- $rc returned from handle.py" >> $logfile
 elif [ -f "/usr/bin/python3.3" ]
 then
     echo "`date`- python3.3 path exists" >> $logfile
     /usr/bin/python3.3 main/handle.py -$1
     rc=$?
-    echo "`date`- $rc returned from handle.py" >> $logfile
 elif [ -f "/usr/bin/python3" ]
 then
     echo "`date`- python3 path exists" >> $logfile
     /usr/bin/python3 main/handle.py -$1
     rc=$?
-    echo "`date`- $rc returned from handle.py" >> $logfile
 elif [ -f "/usr/bin/python2.6" ]
 then
     echo "`date`- python2.6 path exists" >> $logfile
     /usr/bin/python2.6 main/handle.py -$1
     rc=$?
-    echo "`date`- $rc returned from handle.py" >> $logfile
 elif [ -f "/usr/bin/python" ]
 then
     echo "`date`- python path exists" >> $logfile
     /usr/bin/python main/handle.py -$1
     rc=$?
-    echo "`date`- $rc returned from handle.py" >> $logfile
 else
     echo "`date`- python version unknown" >> $logfile
-    exit 3
 fi
+echo "`date`- $rc returned from handle.py" >> $logfile
 
 exit $rc
