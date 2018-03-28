22ad31bbc3e931ae6112b80854354e1fb784912b
Eric Jarvi <ejarvi@users.noreply.github.com>
2018-03-28 12:51:20 -0700

    fix typo in update()

--- VMEncryption/main/handle.py
+++ VMEncryption/main/handle.py
@@ -343,7 +343,7 @@ def update_encryption_settings():
                       message=message)
 
 def update():
-    hutil.do_parse_context('Upadate')
+    hutil.do_parse_context('Update')
     logger.log("Installing pre-requisites")
     DistroPatcher.update_prereq()
     hutil.do_exit(0, 'Update', CommonVariables.extension_success_status, '0', 'Update Succeeded')
