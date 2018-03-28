eab36f736fbf7eb33e2b4dd882ae2da820098c08
Eric Jarvi <ejarvi@users.noreply.github.com>
2018-03-28 12:29:36 -0700

    add centos 7.4 detection

--- VMEncryption/main/handle.py
+++ VMEncryption/main/handle.py
@@ -1560,7 +1560,8 @@ def daemon_encrypt():
                                                          distro_patcher=DistroPatcher,
                                                          logger=logger,
                                                          encryption_environment=encryption_environment)
-        elif ((distro_name == 'centos' and distro_version == '7.3.1611') and
+        elif (((distro_name == 'centos' and distro_version == '7.3.1611') or
+              (distro_name == 'centos' and distro_version.startswith('7.4'))) and 
               (disk_util.is_os_disk_lvm() or os.path.exists('/volumes.lvm'))):
             from oscrypto.rhel_72_lvm import RHEL72LVMEncryptionStateMachine
             os_encryption = RHEL72LVMEncryptionStateMachine(hutil=hutil,
@@ -1570,6 +1571,7 @@ def daemon_encrypt():
         elif ((distro_name == 'redhat' and distro_version == '7.2') or
             (distro_name == 'redhat' and distro_version == '7.3') or
             (distro_name == 'redhat' and distro_version == '7.4') or
+            (distro_name == 'centos' and distro_version.startswith('7.4')) or
             (distro_name == 'centos' and distro_version == '7.3.1611') or
             (distro_name == 'centos' and distro_version == '7.2.1511')):
             from oscrypto.rhel_72 import RHEL72EncryptionStateMachine
--- VMEncryption/main/oscrypto/rhel_72_lvm/encryptstates/PrereqState.py
+++ VMEncryption/main/oscrypto/rhel_72_lvm/encryptstates/PrereqState.py
@@ -46,7 +46,8 @@ class PrereqState(OSEncryptionState):
         self.context.logger.log("Distro info: {0}".format(distro_info))
 
         if (((distro_info[0] == 'centos' and distro_info[1] == '7.3.1611') or
-             (distro_info[0] == 'redhat' and distro_info[1] == '7.3') or
+		     (distro_info[0] == 'centos' and distro_info[1].startswith('7.4')) or
+		     (distro_info[0] == 'redhat' and distro_info[1] == '7.3') or
              (distro_info[0] == 'redhat' and distro_info[1] == '7.4')) and
             self.disk_util.is_os_disk_lvm()):
             self.context.logger.log("Enabling OS volume encryption on {0} {1}".format(distro_info[0],
