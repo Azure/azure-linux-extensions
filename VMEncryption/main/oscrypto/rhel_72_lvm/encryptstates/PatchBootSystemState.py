6a768306ecc76a918b455f8d070dd7278e566093
Ahmad Abdullateef <ahabdull@microsoft.com>
2018-04-05 17:13:18 -0700

    Remove 91lvm rename command

--- VMEncryption/main/oscrypto/rhel_72_lvm/encryptstates/PatchBootSystemState.py
+++ VMEncryption/main/oscrypto/rhel_72_lvm/encryptstates/PatchBootSystemState.py
@@ -215,8 +215,6 @@ class PatchBootSystemState(OSEncryptionState):
         sed_cmd = 'sed -i.bak s/ENCRYPTED_DISK_PARTITION/{0}/ "{1}"'.format(partition, udevaderulepath)
         self.command_executor.Execute(command_to_execute=sed_cmd, raise_exception_on_failure=True)
 
-        self.command_executor.Execute('mv /usr/lib/dracut/modules.d/90lvm /usr/lib/dracut/modules.d/91lvm', True)
-
         self._append_contents_to_file('\nGRUB_CMDLINE_LINUX+=" rd.debug"\n',
                                       '/etc/default/grub')
 
