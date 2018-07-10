9e17fb24e7724be7fa8061df9c2e40124cb324eb
Abhijeet Gaiha <agaiha@microsoft.com>
2018-07-10 17:45:59 +0530

    Update omsagent.py

--- OmsAgent/omsagent.py
+++ OmsAgent/omsagent.py
@@ -40,7 +40,7 @@ except Exception as e:
 
 # Global Variables
 PackagesDirectory = 'packages'
-BundleFileName = 'omsagent-1.6.0-42.universal.x64.sh'
+BundleFileName = 'omsagent-1.6.0-139.universal.x64.sh'
 GUIDRegex = r'[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}'
 GUIDOnlyRegex = r'^' + GUIDRegex + '$'
 SCOMCertIssuerRegex = r'^[\s]*Issuer:[\s]*CN=SCX-Certificate/title=SCX' + GUIDRegex + ', DC=.*$'
