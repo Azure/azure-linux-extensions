7d6b42e68f627f2d893cae63a5987ce2c16402a6
shpimpal <34675111+shpimpal@users.noreply.github.com>
2018-08-24 11:39:41 +0530

    Update watcherutil.py

--- OmsAgent/watcherutil.py
+++ OmsAgent/watcherutil.py
@@ -173,7 +173,7 @@ class Watcher:
                         {{
 
             			"name": "Version",
-            			"value": "1.7.3"
+            			"value": "1.7.0.6"
              		}},
 
                         {{
