779b31938fadbc0a6f3bd8f07f863d2f50c5838a
Siddhartha Mathiharan <42589365+simathih@users.noreply.github.com>
2018-11-02 16:00:38 -0700

    Update watcherutil.py

--- OmsAgent/watcherutil.py
+++ OmsAgent/watcherutil.py
@@ -173,7 +173,7 @@ class Watcher:
                         {{
 
             			"name": "Version",
-            			"value": "1.8.1"
+            			"value": "1.8.3"
              		}},
 
                         {{
