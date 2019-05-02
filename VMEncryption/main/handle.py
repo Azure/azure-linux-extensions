99e383e393a7ef8924c2a2125eb3f3338770c36b
Shiv Verma <shiverma@microsoft.com>
2019-05-02 11:35:28 -0700

    address comments

--- VMEncryption/main/handle.py
+++ VMEncryption/main/handle.py
@@ -245,14 +245,15 @@ def update_encryption_settings():
             logger.log("New key successfully added to all encrypted devices")
 
             if DistroPatcher.distro_info[0] == "Ubuntu":
+                logger.log("Updating initrd image with new osluksheader.")
                 executor.Execute("update-initramfs -u -k all", True)
 
             if DistroPatcher.distro_info[0] == "redhat" or DistroPatcher.distro_info[0] == "centos":
                 distro_version = DistroPatcher.distro_info[1]
 
                 if distro_version.startswith('7.'):
+                    logger.log("Updating initrd image with new osluksheader.")
                     executor.ExecuteInBash("/usr/sbin/dracut -f -v --kver `grubby --default-kernel | sed 's|/boot/vmlinuz-||g'`", True)
-                    logger.log("Update initrd image with new osluksheader.")
 
             os.unlink(temp_keyfile.name)
 
@@ -329,14 +330,14 @@ def update_encryption_settings():
             logger.log("Old key successfully removed from all encrypted devices")
 
             if DistroPatcher.distro_info[0] == "Ubuntu":
-                logger.log("Updateing initrd image with new osluksheader.")
+                logger.log("Updating initrd image with new osluksheader.")
                 executor.Execute("update-initramfs -u -k all", True)
 
             if DistroPatcher.distro_info[0] == "redhat" or DistroPatcher.distro_info[0] == "centos":
                 distro_version = DistroPatcher.distro_info[1]
 
                 if distro_version.startswith('7.'):
-                    logger.log("Updateing initrd image with new osluksheader.")
+                    logger.log("Updating initrd image with new osluksheader.")
                     executor.ExecuteInBash("/usr/sbin/dracut -f -v --kver `grubby --default-kernel | sed 's|/boot/vmlinuz-||g'`", True)
 
             hutil.save_seq()
