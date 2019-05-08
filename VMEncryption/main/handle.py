741c9cb0cf39b32d91f7d68a216227b3626d7391
Vishal Mishra <vimish@microsoft.com>
2019-05-08 15:35:57 -0700

    Used space encryption and CentOS 7.6 support

--- VMEncryption/main/handle.py
+++ VMEncryption/main/handle.py
@@ -1595,7 +1595,8 @@ def daemon_encrypt():
                                                             encryption_environment=encryption_environment)
         elif (((distro_name == 'centos' and distro_version == '7.3.1611') or
                (distro_name == 'centos' and distro_version.startswith('7.4')) or
-               (distro_name == 'centos' and distro_version.startswith('7.5'))) and
+               (distro_name == 'centos' and distro_version.startswith('7.5')) or 
+               (distro_name == 'centos' and distro_version.startswith('7.6'))) and
               (disk_util.is_os_disk_lvm() or os.path.exists('/volumes.lvm'))):
             from oscrypto.rhel_72_lvm import RHEL72LVMEncryptionStateMachine
             os_encryption = RHEL72LVMEncryptionStateMachine(hutil=hutil,
@@ -1607,6 +1608,7 @@ def daemon_encrypt():
               (distro_name == 'redhat' and distro_version == '7.4') or
               (distro_name == 'redhat' and distro_version == '7.5') or
               (distro_name == 'redhat' and distro_version == '7.6') or
+              (distro_name == 'centos' and distro_version.startswith('7.6')) or
               (distro_name == 'centos' and distro_version.startswith('7.5')) or
               (distro_name == 'centos' and distro_version.startswith('7.4')) or
               (distro_name == 'centos' and distro_version == '7.3.1611') or
