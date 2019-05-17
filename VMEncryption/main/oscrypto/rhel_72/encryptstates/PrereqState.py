94ef3385538dfcf3b586b207da06d98a6cf8f7d4
Vishal Mishra <vimish@microsoft.com>
2019-05-17 11:01:55 -0700

    Used space encryption and CentOS 7.6 support (#802)

--- VMEncryption/main/oscrypto/rhel_72/encryptstates/PrereqState.py
+++ VMEncryption/main/oscrypto/rhel_72/encryptstates/PrereqState.py
@@ -51,6 +51,7 @@ class PrereqState(OSEncryptionState):
            (distro_info[0] == 'redhat' and distro_info[1] == '7.4') or
            (distro_info[0] == 'redhat' and distro_info[1] == '7.5') or
            (distro_info[0] == 'redhat' and distro_info[1] == '7.6') or
+           (distro_info[0] == 'centos' and distro_info[1].startswith('7.6')) or
            (distro_info[0] == 'centos' and distro_info[1].startswith('7.5')) or
            (distro_info[0] == 'centos' and distro_info[1].startswith('7.4')) or
            (distro_info[0] == 'centos' and distro_info[1] == '7.3.1611') or
