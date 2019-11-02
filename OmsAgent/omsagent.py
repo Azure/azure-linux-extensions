278ebc070323ac0b384f7cad62a16f72efc19dd4
scxsvc@microsoft.com <scxsvc@microsoft.com>
2019-11-01 18:15:19 -0700

    omsagent extension version update

--- OmsAgent/omsagent.py
+++ OmsAgent/omsagent.py
@@ -46,7 +46,7 @@ except Exception as e:
 
 # Global Variables
 PackagesDirectory = 'packages'
-BundleFileName = 'omsagent-1.12.7-0.universal.x64.sh'
+BundleFileName = 'omsagent-1.12.9-0.universal.x64.sh'
 GUIDRegex = r'[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}'
 GUIDOnlyRegex = r'^' + GUIDRegex + '$'
 SCOMCertIssuerRegex = r'^[\s]*Issuer:[\s]*CN=SCX-Certificate/title=SCX' + GUIDRegex + ', DC=.*$'
