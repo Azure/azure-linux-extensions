4f78611ea330b1a86e9c3797f64f13fc7db0daea
Siddhartha Mathiharan <simathih@microsoft.com>
2020-01-22 14:49:12 -0800

    add first party support

--- AzureMonitorAgent/agent.py
+++ AzureMonitorAgent/agent.py
@@ -51,8 +51,8 @@ except Exception as e:
 # Global Variables
 PackagesDirectory = 'packages'
 # TO BE CHANGED WITH EACH NEW RELEASE IF THE BUNDLE VERSION CHANGES
-BundleFileNameDeb = 'azure-mdsd_1.5.119-build.dev_x86_64.deb'
-BundleFileNameRpm = 'azure-mdsd_1.5.119-build.dev_x86_64.rpm'
+BundleFileNameDeb = 'azure-mdsd_1.5.119-build.develop.805_x86_64.deb'
+BundleFileNameRpm = 'azure-mdsd_1.5.119-build.develop.805_x86_64.rpm'
 BundleFileName = ''
 InitialRetrySleepSeconds = 30
 PackageManager = ''
@@ -188,10 +188,14 @@ def install():
     find_package_manager("Install")
     exit_if_vm_not_supported('Install')
 
-    # public_settings, protected_settings = get_settings()
+    public_settings, protected_settings = get_settings()
+    
     # if public_settings is None:
     #     raise ParameterMissingException('Public configuration must be ' \
     #                                     'provided')
+    #public_settings.get('workspaceId')
+    #protected_settings.get('workspaceKey')
+    
     package_directory = os.path.join(os.getcwd(), PackagesDirectory)
     bundle_path = os.path.join(package_directory, BundleFileName)
     os.chmod(bundle_path, 100)
@@ -204,14 +208,75 @@ def install():
                                          retry_check = retry_if_dpkg_locked,
                                          final_check = final_check_if_dpkg_locked)
 
-    default_configs = {
-        "ENABLE_MCS" : "true",
-        "MCS_ENDPOINT" : "eastus.amcs.ppe.control.monitor.azure.com",
+    default_configs = {        
+        "MCS_ENDPOINT" : "amcs.control.monitor.azure.com",
         "AZURE_ENDPOINT" : "https://management.azure.com/",
-        "ADD_REGION_TO_MCS_ENDPOINT" : "false",
-        "OMS_TLD" : "int2.microsoftatlanta-int.com",
-        "customResourceId" : "/subscriptions/42e7aed6-f510-46a2-8597-a5fe2e15478b/resourcegroups/amcs-test/providers/Microsoft.OperationalInsights/workspaces/amcs-pretend-linuxVM"
+        "ADD_REGION_TO_MCS_ENDPOINT" : "true",
+        #"OMS_TLD" : "int2.microsoftatlanta-int.com",
+        #"customResourceId" : "/subscriptions/42e7aed6-f510-46a2-8597-a5fe2e15478b/resourcegroups/amcs-test/providers/Microsoft.OperationalInsights/workspaces/amcs-pretend-linuxVM",
+        #"ENABLE_MCS" : "true",
     }
+
+    # decide the mode
+    if protected_settings is None:
+        default_configs["ENABLE_MCS"] = "true"
+    else:
+        # look for LA protected settings
+        for var in protected_settings.keys():
+            if "_key" in var:
+                default_configs[var] = protected_settings.get(var)
+        
+        # check if required GCS params are available
+        MONITORING_GCS_CERT_CERTFILE = None
+        if protected_settings.has_key("certificate"):
+            MONITORING_GCS_CERT_CERTFILE = base64.standard_b64decode(protected_settings.get("certificate"))
+
+        MONITORING_GCS_CERT_KEYFILE = None
+        if protected_settings.has_key("certificateKey"):
+            MONITORING_GCS_CERT_KEYFILE = base64.standard_b64decode(protected_settings.get("certificateKey"))
+
+        MONITORING_GCS_ENVIRONMENT = ""
+        if protected_settings.has_key("monitoringGCSEnvironment"):
+            MONITORING_GCS_ENVIRONMENT = protected_settings.get("monitoringGCSEnvironment")
+
+        MONITORING_GCS_NAMESPACE = ""
+        if protected_settings.has_key("namespace"):
+            MONITORING_GCS_NAMESPACE = protected_settings.get("namespace")
+
+        MONITORING_GCS_ACCOUNT = ""
+        if protected_settings.has_key("monitoringGCSAccount"):
+            MONITORING_GCS_ACCOUNT = protected_settings.get("monitoringGCSAccount")
+
+        MONITORING_GCS_REGION = ""
+        if protected_settings.has_key("monitoringGCSRegion"):
+            MONITORING_GCS_REGION = protected_settings.get("monitoringGCSRegion")
+
+        MONITORING_CONFIG_VERSION = ""
+        if protected_settings.has_key("configVersion"):
+            MONITORING_CONFIG_VERSION = protected_settings.get("configVersion")
+
+        if MONITORING_GCS_CERT_CERTFILE is None or MONITORING_GCS_CERT_KEYFILE is None or MONITORING_GCS_ENVIRONMENT is "" or MONITORING_GCS_NAMESPACE is "" or MONITORING_GCS_ACCOUNT is "" or MONITORING_GCS_REGION is "" or MONITORING_CONFIG_VERSION is "":
+            default_configs["ENABLE_MCS"] = "true"
+        else:
+            # set the values for GCS
+            default_configs["MONITORING_USE_GENEVA_CONFIG_SERVICE"] = "true"        
+            default_configs["MONITORING_GCS_ENVIRONMENT"] = MONITORING_GCS_ENVIRONMENT
+            default_configs["MONITORING_GCS_NAMESPACE"] = MONITORING_GCS_NAMESPACE
+            default_configs["MONITORING_GCS_ACCOUNT"] = MONITORING_GCS_ACCOUNT
+            default_configs["MONITORING_GCS_REGION"] = MONITORING_GCS_REGION
+            default_configs["MONITORING_CONFIG_VERSION"] = MONITORING_CONFIG_VERSION
+            default_configs["MONITORING_GCS_CERT_CERTFILE"] = "/etc/mdsd.d/gcscert.pem"
+            default_configs["MONITORING_GCS_CERT_KEYFILE"] = "/etc/mdsd.d/gcskey.pem"
+
+            # write the certificate and key to disk
+            fh = open("/etc/mdsd.d/gcscert.pem", "wb")
+            fh.write(MONITORING_GCS_CERT_CERTFILE)
+            fh.close()
+
+            fh = open("/etc/mdsd.d/gcskey.pem", "wb")
+            fh.write(MONITORING_GCS_CERT_KEYFILE)
+            fh.close()
+
     config_file = "/etc/default/mdsd"
     config_updated = False
     try:
