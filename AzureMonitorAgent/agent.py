da538103df361093802a1c15506e382a7d89e012
Siddhartha Mathiharan <42589365+simathih@users.noreply.github.com>
2020-01-30 09:29:18 -0800

    Update agent.py

--- AzureMonitorAgent/agent.py
+++ AzureMonitorAgent/agent.py
@@ -51,8 +51,8 @@ except Exception as e:
 # Global Variables
 PackagesDirectory = 'packages'
 # TO BE CHANGED WITH EACH NEW RELEASE IF THE BUNDLE VERSION CHANGES
-BundleFileNameDeb = 'azure-mdsd_1.5.119-build.dev_x86_64.deb'
-BundleFileNameRpm = 'azure-mdsd_1.5.119-build.dev_x86_64.rpm'
+BundleFileNameDeb = 'azure-mdsd_1.5.119-build.develop.825_x86_64.deb'
+BundleFileNameRpm = 'azure-mdsd_1.5.119-build.develop.825_x86_64.rpm'
 BundleFileName = ''
 InitialRetrySleepSeconds = 30
 PackageManager = ''
