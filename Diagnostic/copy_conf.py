24eab8a4bf7e2ed099c21e13050feec6c45566a2
root <nidhanda@microsoft.com>
2020-02-26 16:33:04 -0800

    Added diff operation functionality in config parsing

--- Diagnostic/copy_conf.py
+++ Diagnostic/copy_conf.py
@@ -41,11 +41,11 @@ name_map = {
 # "percentusedinodes" : {"plugin":"disk", "field":"inodes_used"}, #Need to calculate percentage
 
 # "transferspersecond" : {"plugin":"diskio", "field":"reads + writes"}, #Need to calculate sum
-"bytesreadpersecond" : {"plugin":"diskio", "field":"read_bytes", "extra":"diff"}, #Need to calculate diff (but each second not each interval)
+"bytesreadpersecond" : {"plugin":"diskio", "field":"read_bytes", "op":"diff"}, #Need to calculate diff (but each second not each interval)
 # "bytespersecond" : {"plugin":"diskio", "field":"read_bytes + write_bytes"}, #Need to calculate diff and then sum
-"byteswrittenpersecond" : {"plugin":"diskio", "field":"write_bytes", "extra":"diff"}, #Need to calculate diff (but each second not each interval)
-"readspersecond" : {"plugin":"diskio", "field":"reads", "extra":"diff"}, #Need to calculate diff (but each second not each interval)
-"writespersecond" : {"plugin":"diskio", "field":"writes", "extra":"diff"}, #Need to calculate diff (but each second not each interval)
+"byteswrittenpersecond" : {"plugin":"diskio", "field":"write_bytes", "op":"diff"}, #Need to calculate diff (but each second not each interval)
+"readspersecond" : {"plugin":"diskio", "field":"reads", "op":"diff"}, #Need to calculate diff (but each second not each interval)
+"writespersecond" : {"plugin":"diskio", "field":"writes", "op":"diff"}, #Need to calculate diff (but each second not each interval)
 
 #OMI Disk plugin 
 # "readbytespersecond" :
@@ -93,6 +93,10 @@ with open(settings, 'r') as f:
                 telegraf_conf[plugin]["fields"][name_map[counter]["field"]] = {}
                 telegraf_conf[plugin]["fields"][name_map[counter]["field"]]["displayName"] = item["annotation"][0]["displayName"]
                 telegraf_conf[plugin]["fields"][name_map[counter]["field"]]["interval"] = item["sampleRate"][2:].lower() #Example, converting PT15S tp 15s
+                if "op" in name_map[counter]:
+                    telegraf_conf[plugin]["fields"][name_map[counter]["field"]]["op"] = name_map[counter]["op"]
+                # else:
+                #     telegraf_conf[plugin]["fields"][name_map[counter]["field"]]["op"] = ""
 
 print telegraf_conf
 
@@ -100,39 +104,48 @@ print telegraf_conf
 Sample converted telegraf conf dict -
 
 {
-    'net': {
-        'fields': {
-            'err_out': {'interval': u'15s', 'displayName': u'Packets sent errors'},
-            'packets_sent': {'interval': u'15s', 'displayName': u'Packets sent'}, 
-            'err_in': {'interval': u'15s', 'displayName': u'Packets received errors'}, 
-            'bytes_sent': {'interval': u'15s', 'displayName': u'Network out guest OS'}, 
-            'packets_recv': {'interval': u'5s', 'displayName': u'Packets received'}
-        }, 
-        'displayName': u'network'
-    }, 
-    'cpu': {
-        'fields': {
-            'usage_nice': {'interval': u'15s', 'displayName': u'CPU nice time'}, 
-            'usage_user': {'interval': u'15s', 'displayName': u'CPU user time'}, 
-            'usage_idle': {'interval': u'15s', 'displayName': u'CPU idle time'}, 
-            'usage_active': {'interval': u'15s', 'displayName': u'CPU percentage guest OS'}, 
-            'usage_irq': {'interval': u'15s', 'displayName': u'CPU interrupt time'}, 
-            'usage_system': {'interval': u'15s', 'displayName': u'CPU privileged time'}
-        }, 
-        'displayName': u'processor'
-    }
-}
+  'mem': {
+    'fields': {
+      'used_percent': {  'interval': u'15s',  'displayName': u'Memory percentage'},
+      'used': {  'interval': u'15s',  'displayName': u'Memory used'},
+      'available_percent': {  'interval': u'15s',  'displayName': u'Mem. percent available'}
+    },
+    'displayName': u'memory'
+  },
+  'diskio': {
+    'fields': {
+      'reads': {  'interval': u'15s',  'displayName': u'Disk reads',  'op': 'diff'},
+      'writes': {  'interval': u'15s',  'displayName': u'Disk writes',  'op': 'diff'},
+      'write_bytes': {  'interval': u'15s',  'displayName': u'Filesystem write bytes/sec',  'op': 'diff'}
+    },
+    'displayName': u'filesystem'
+  },
+  'net': {
+    'fields': {
+      'err_out': {  'interval': u'15s',  'displayName': u'Packets sent errors'},
+      'packets_sent': {  'interval': u'15s',  'displayName': u'Packets sent'},
+      'err_in': {  'interval': u'15s',  'displayName': u'Packets received errors'},
+      'bytes_sent': {  'interval': u'15s',  'displayName': u'Network out guest OS'},
+      'packets_recv': {  'interval': u'5s',  'displayName': u'Packets received'}
+    },
+    'displayName': u'network'
+  },
 """
+
 input_str = ""
-rename_str= ""
+rename_str = "[[processors.rename]]\n"
+aggregator_str = ""
 for plugin in telegraf_conf:
     min_interval = "60s"
     input_str += "[[inputs." + plugin + "]]\n"
-    rename_str += "[[processors.rename]]\n"
     rename_str += "\n" + " "*2 + "[[processors.rename.replace]]\n"
     rename_str += " "*4 + "measurement = \"" + str(plugin) +"\"\n" 
     rename_str += " "*4 + "dest = \"" + telegraf_conf[plugin]["displayName"] +"\"\n"
     fields = ""
+    ops_fields = ""
+    ops = ""
+    twiceminperiod = ""
+    aggregate = False
     for field in telegraf_conf[plugin]["fields"]:
         fields += "\"" + field + "\", "
         
@@ -146,10 +159,27 @@ for plugin in telegraf_conf:
         rename_str += " "*4 + "field = \"" + field + "\"\n"
         rename_str += " "*4 + "dest = \"" + telegraf_conf[plugin]["fields"][field]["displayName"] + "\"\n"
 
+        #compute values for aggregator options
+        if "op" in telegraf_conf[plugin]["fields"][field]:
+            aggregate = True
+            if telegraf_conf[plugin]["fields"][field]["op"] not in ops:
+                ops += "\"" +  telegraf_conf[plugin]["fields"][field]["op"] + "\", "
+            ops_fields += "\"" +  telegraf_conf[plugin]["fields"][field]["displayName"] + "\", "
+            twiceminperiod = str(int(min_interval[:-1])*2)
+
+
+    #Add respective operations for aggregators
+    if aggregate:
+        aggregator_str += "[[aggregators.basicstats]]\n"
+        aggregator_str += " "*2 + "period = \"" + twiceminperiod + "s\"\n"
+        aggregator_str += " "*2 + "drop_original = false\n"
+        aggregator_str += " "*2 + "fieldpass = [" + ops_fields[:-2] + "]\n" #-2 to strip the last comma and space
+        aggregator_str += " "*2 + "stats = [" + ops[:-2] + "]\n\n"  #-2 to strip the last comma and space
+
 
     rename_str += "\n"
-    input_str += "\n  fieldpass = ["+fields[:-2]+"]\n"  #Using fields[: -2] here to get rid of the last ", " at the end of the string
-    input_str += "  interval = " + "\"" + min_interval + "\"\n"
+    input_str += " "*2 + "fieldpass = ["+fields[:-2]+"]\n"  #Using fields[: -2] here to get rid of the last ", " at the end of the string
+    input_str += " "*2 + "interval = " + "\"" + min_interval + "\"\n\n"
 
 
 
@@ -158,6 +188,9 @@ with open("./telegraf.conf", "w") as f:
     f.write("\n")
     f.write(rename_str)
     f.write("\n")
+    if len(aggregator_str) > 0:
+        f.write(aggregator_str)
+        f.write("\n")
 """
 Sample telegraf TOML file output
 
@@ -170,5 +203,21 @@ Sample telegraf TOML file output
 
   fieldpass = ["usage_nice", "usage_user", "usage_idle", "usage_active", "usage_irq", "usage_system"]
   interval = "15s"
+
+[[processors.rename]]
+
+  [[processors.rename.replace]]
+    measurement = "net"
+    dest = "network"
+
+  [[processors.rename.replace]]
+    field = "err_out"
+    dest = "Packets sent errors"
+
+[[aggregators.basicstats]]
+  period = "30s"
+  drop_original = false
+  fieldpass = ["Disk reads", "Disk writes", "Filesystem write bytes/sec"]
+  stats = ["diff"]
 
 """
