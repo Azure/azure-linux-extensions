4b48c1d4cbe8234b5f46cd913038e9eb86a6a23e
Ivan Pavicevic <ivanp@microsoft.com>
2020-03-02 14:20:53 -0800

    Unittest: reading expected values from the JSON file.

--- VMEncryption/test/test_common.py
+++ VMEncryption/test/test_common.py
@@ -4,8 +4,11 @@ import main.Common
 import json
 
 class TestCommonVariables(unittest.TestCase):
+    def setUp(self):
+        file_path = os.path.join(os.path.dirname(main.Common.__file__), "common_parameters.json")
+        with open(file_path, "r") as content:
+            self.expected_common_parameters = json.load(content)
     
     def test_common_variables(self):
-        self.assertEqual(main.Common.CommonVariables.extension_name, "AzureDiskEncryptionForLinux")
-        self.assertEqual(main.Common.CommonVariables.extension_version, "1.1.0.47")
-
+        self.assertEqual(main.Common.CommonVariables.extension_name, self.expected_common_parameters["extension_name"])
+        self.assertEqual(main.Common.CommonVariables.extension_version, self.expected_common_parameters["extension_version"])
