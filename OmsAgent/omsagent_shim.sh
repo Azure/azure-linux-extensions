8ac15b7054eb0efdb6e8e700223bba7fa1a7e2b4
Henry Stolz <hestolz@microsoft.com>
2020-03-20 10:48:24 -0700

    add python-future as submodule

--- OmsAgent/omsagent_shim.sh
+++ OmsAgent/omsagent_shim.sh
@@ -18,17 +18,6 @@ function find_python() {
     fi
 }
 
-# Usage: run_command "shell command" "description of action"
-function run_command() {
-    eval $1
-    if [ $? != 0 ]; then
-        echo "$2 failed, command: $1" >&2
-        exit 52
-    else
-        echo "$2 succeeded"
-    fi
-}
-
 find_python PYTHON
 
 if [ -z "$PYTHON" ]
@@ -39,42 +28,5 @@ else
     ${PYTHON} --version
 fi
 
-# Install python-future dependency required for omsagent.py.
-# Infer distro and use the appropriate package manager,
-# falling back on direct package manager detection
-ACTION="python-future install"
-if [ -f "/etc/debian_version" ]; then # Ubuntu, Debian
-    dpkg-query -l python-future | grep ^ii
-    if [ $? != 0 ]; then
-        run_command "apt-get update" "python-future preinstall"
-        run_command "apt-get install -y python-future" $ACTION
-    fi
-elif [ -f "/etc/redhat-release" ]; then # RHEL, CentOS, Oracle
-    rpm -qi python-future
-    if [ $? != 0 ]; then
-        run_command "yum install -y python-future" $ACTION
-    fi
-elif [ -f "/etc/os-release" ]; then # Possibly SLES, openSUSE
-    grep PRETTY_NAME /etc/os-release | sed 's/PRETTY_NAME=//g' | tr -d '="' | grep -i suse
-    if [ $? != 0 ]; then
-        echo "Unsupported or indeterminable operating system" >&2
-        exit 51
-    fi
-    rpm -qi python-future
-    if [ $? != 0 ]; then
-        run_command "zypper --non-interactive install python-future" $ACTION
-    fi
-elif [ -x "$(command -v apt-get)" ]; then
-    run_command "apt-get update" "python-future preinstall"
-    run_command "apt-get install -y python-future" $ACTION
-elif [ -x "$(command -v yum)" ]; then
-    run_command "yum install -y python-future" $ACTION
-elif [ -x "$(command -v zypper)" ]; then
-    run_command "zypper --non-interactive install python-future" $ACTION
-else
-    echo "Unsupported or indeterminable operating system" >&2
-    exit 51
-fi
-
-${PYTHON} ${COMMAND} ${ARG}
+PYTHONPATH=${PWD}/ext/future:${PYTHONPATH} ${PYTHON} ${COMMAND} ${ARG}
 exit $?
