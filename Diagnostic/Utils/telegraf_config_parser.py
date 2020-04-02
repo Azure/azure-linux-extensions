3ad03f1ee1baa981a3c460256f622d896133eb9b
nidhanda <nidhanda@microsoft.com>
2020-04-02 00:42:17 -0700

    Splitting the config parser for efficient code sharing

--- Diagnostic/Utils/telegraf_config_parser.py
+++ Diagnostic/Utils/telegraf_config_parser.py
@@ -1,102 +1,42 @@
 import json
+import os
+from Utils.telegraf_name_map import name_map
 
-name_map = { 
-"percentiowaittime" : {"plugin":"cpu", "field":"usage_iowait"},
-"percentusertime" : {"plugin":"cpu", "field":"usage_user"},
-"percentnicetime" : {"plugin":"cpu", "field":"usage_nice"},
-"percentprocessortime" : {"plugin":"cpu", "field":"usage_active"},
-"percentinterrupttime" : {"plugin":"cpu", "field":"usage_irq"},
-"percentidletime" : {"plugin":"cpu", "field":"usage_idle"},
-"percentprivilegedtime" : {"plugin":"cpu", "field":"usage_system"},
-
-"bytesreceived" : {"plugin":"net", "field":"bytes_recv"},
-# "bytestotal" : {"plugin":"net", "field":"butes_recv + bytes_sent"}, #Need to calculate sum
-"bytestransmitted" : {"plugin":"net", "field":"bytes_sent"},
-# "totalcollisions" : {"plugin":"net", "field":"drop_in + drop_out"}, #Need to calculate sum
-"totalrxerrors" : {"plugin":"net", "field":"err_in"},
-"packetstransmitted" : {"plugin":"net", "field":"packets_sent"},
-"packetsreceived" : {"plugin":"net", "field":"packets_recv"},
-"totaltxerrors" : {"plugin":"net", "field":"err_out"},
-
-"availablememory" : {"plugin":"mem", "field":"available"},
-"percentavailablememory" : {"plugin":"mem", "field":"available_percent"},
-"usedmemory" : {"plugin":"mem", "field":"used"},
-"percentusedmemory" : {"plugin":"mem", "field":"used_percent"}, 
-
-"availableswap" : {"plugin":"swap", "field":"free"},
-# "percentavailableswap" : {"plugin":"swap", "field":"available"}, #Need to calculate percentage
-"usedswap" : {"plugin":"swap", "field":"used"}, 
-"percentusedswap" : {"plugin":"swap", "field":"used_percent"},
-
-"pagesreadpersec": {"plugin":"kernel_vmstat", "field":"pgpgin", "op":"diff"},
-"pageswrittenpersec" : {"plugin":"kernel_vmstat", "field":"pgpgout", "op":"diff"},
-# "pagespersec" : {"plugin":"kernel", "field":""},
-
-#OMI Filesystem plugin
-"usedspace" : {"plugin":"disk", "field":"used"},
-"percentusedspace" : {"plugin":"disk", "field":"used_percent"},
-"freespace" : {"plugin":"disk", "field":"free"},
-# "percentfreespace" : {"plugin":"disk", "field":"disk_pages_in"}, #Need to calculate percentage
-# "percentfreeinodes" : {"plugin":"disk", "field":"inodes_free"}, #Need to calculate percentage
-# "percentusedinodes" : {"plugin":"disk", "field":"inodes_used"}, #Need to calculate percentage
-
-# "transferspersecond" : {"plugin":"diskio", "field":"reads + writes"}, #Need to calculate sum
-"bytesreadpersecond" : {"plugin":"diskio", "field":"read_bytes", "op":"diff"}, #Need to calculate diff (but each second not each interval)
-# "bytespersecond" : {"plugin":"diskio", "field":"read_bytes + write_bytes"}, #Need to calculate diff and then sum
-"byteswrittenpersecond" : {"plugin":"diskio", "field":"write_bytes", "op":"diff"}, #Need to calculate diff (but each second not each interval)
-"readspersecond" : {"plugin":"diskio", "field":"reads", "op":"diff"}, #Need to calculate diff (but each second not each interval)
-"writespersecond" : {"plugin":"diskio", "field":"writes", "op":"diff"}, #Need to calculate diff (but each second not each interval)
-
-#OMI Disk plugin 
-"readbytespersecond" : {"plugin":"diskio", "field":"read_bytes", "op":"diff"},
-"writebytespersecond" : {"plugin":"diskio", "field":"write_bytes", "op":"diff"},
-# "bytespersecond" : {"plugin":"diskio", "field":"read_bytes + write_bytes"},
-"readspersecond" : {"plugin":"diskio", "field":"reads", "op":"diff"}, #Need to calculate diff (but each second not each interval)
-"writespersecond" : {"plugin":"diskio", "field":"writes", "op":"diff"},
-# "transferspersecond" :
-"averagereadtime" : {"plugin":"diskio", "field":"read_time"},
-"averagewritetime" : {"plugin":"diskio", "field":"write_time"},
-"averagetransfertime" :{"plugin":"diskio", "field":"io_time"},
-# "averagediskqueuelength" : 
-
-}
 
 """
-Sample OMI metric json config taken from .settings file
-{
-    u'counterSpecifier': u'/builtin/network/packetstransmitted', 
-    u'counter': u'packetstransmitted', 
-    u'class': u'network', 
-    u'sampleRate': u'PT15S', 
-    u'type': u'builtin', 
-    u'annotation': [{
-            u'locale': u'en-us', 
-            u'displayName': u'Packets sent'
-        }],
-    u'unit': u'Count'
-}
+Sample input data received by this script
+[
+    {
+        "displayName" : "Network/Packets sent",
+        "interval" : "15s"
+    },
+    {
+        "displayName" : "Network/Packets recieved",
+        "interval" : "15s"
+    }
+]
 """
+
 
 def parse_config(data):
 
-    if "performanceCounterConfiguration" not in data or len(data["performanceCounterConfiguration"]) == 0:
-        return "", ""
+    if len(data) == 0:
+        return []
 
     telegraf_json = {}
-    perfconf = data["performanceCounterConfiguration"]
 
-    for item in perfconf:
-        counter = item["counter"]
+    for item in data:
+        counter = item["displayName"]
         if counter in name_map:
             plugin = name_map[counter]["plugin"]
-            omiclass = item["class"]
+            omiclass = counter.split("->")[0]
             if omiclass not in telegraf_json:
                 telegraf_json[omiclass] = {}
             if plugin not in telegraf_json[omiclass]:
                 telegraf_json[omiclass][plugin] = {}
             telegraf_json[omiclass][plugin][name_map[counter]["field"]] = {}
-            telegraf_json[omiclass][plugin][name_map[counter]["field"]]["displayName"] = item["annotation"][0]["displayName"]
-            telegraf_json[omiclass][plugin][name_map[counter]["field"]]["interval"] = item["sampleRate"][2:].lower() #Example, converting PT15S tp 15s
+            telegraf_json[omiclass][plugin][name_map[counter]["field"]]["displayName"] = counter.split("->")[1]
+            telegraf_json[omiclass][plugin][name_map[counter]["field"]]["interval"] = item["interval"]
             if "op" in name_map[counter]:
                 telegraf_json[omiclass][plugin][name_map[counter]["field"]]["op"] = name_map[counter]["op"]
 
@@ -105,43 +45,51 @@ def parse_config(data):
 
     u'network': {
         'net': {
-        'err_in': {  'interval': u'15s',  'displayName': u'Packets received errors'},
-        'packets_sent': {  'interval': u'15s',  'displayName': u'Packets sent'},
-        'bytes_recv': {  'interval': u'5s',  'displayName': u'Network in guest OS'},
-        'packets_recv': {  'interval': u'5s',  'displayName': u'Packets received'},
-        'err_out': {  'interval': u'15s',  'displayName': u'Packets sent errors'},
-        'bytes_sent': {  'interval': u'15s',  'displayName': u'Network out guest OS'}
+            'err_in': {  'interval': u'15s',  'displayName': u'Packets received errors'},
+            'packets_sent': {  'interval': u'15s',  'displayName': u'Packets sent'},
+            'bytes_recv': {  'interval': u'5s',  'displayName': u'Network in guest OS'},
+            'packets_recv': {  'interval': u'5s',  'displayName': u'Packets received'},
+            'err_out': {  'interval': u'15s',  'displayName': u'Packets sent errors'},
+            'bytes_sent': {  'interval': u'15s',  'displayName': u'Network out guest OS'}
         }
     },
     u'filesystem': {
         'disk': {
-        'used_percent': {  'interval': u'15s',  'displayName': u'Filesystem % used space'},
-        'used': {  'interval': u'15s',
-            'displayName': u'Filesystem used space'},
-        'free': {  'interval': u'15s',  'displayName': u'Filesystem free space'}
+            'used_percent': {  'interval': u'15s',  'displayName': u'Filesystem % used space'},
+            'used': {  'interval': u'15s',
+                'displayName': u'Filesystem used space'},
+            'free': {  'interval': u'15s',  'displayName': u'Filesystem free space'}
         },
         'diskio': {
-        'write_bytes': {  'interval': u'15s',  'displayName': u'Filesystem write bytes/sec',  'op': 'diff'},
-        'read_bytes': {  'interval': u'15s',  'displayName': u'Filesystem read bytes/sec',  'op': 'diff'},
-        'writes': {  'interval': u'15s',  'displayName': u'Filesystem writes/sec',  'op': 'diff'},
-        'reads': {  'interval': u'15s',  'displayName': u'Filesystem reads/sec',  'op': 'diff'}
+            'write_bytes': {  'interval': u'15s',  'displayName': u'Filesystem write bytes/sec',  'op': 'rate'},
+            'read_bytes': {  'interval': u'15s',  'displayName': u'Filesystem read bytes/sec',  'op': 'rate'},
+            'writes': {  'interval': u'15s',  'displayName': u'Filesystem writes/sec',  'op': 'rate'},
+            'reads': {  'interval': u'15s',  'displayName': u'Filesystem reads/sec',  'op': 'rate'}
         }
     },
     """
 
     if len(telegraf_json) == 0:
         raise Exception("Unable to parse telegraf config into intermediate dictionary.")
-        return "", ""
+        return []
 
-    input_str = ""
-    rename_str = "[[processors.rename]]\n"
-    aggregator_str = ""
+
+
+    int_file = {"filename":"intermediate.json", "data": json.dumps(telegraf_json)}
+    output = []
+    output.append(int_file)
 
     for omiclass in telegraf_json:
+        input_str = ""
+        rename_str = ""
+        aggregator_str = ""
         for plugin in telegraf_json[omiclass]:
+            config_file = {"filename" : omiclass+".conf"}
             min_interval = "60s"
             input_str += "[[inputs." + plugin + "]]\n"
-            input_str += " "*2 + "name_override = \"" + omiclass + "\"\n"
+            # input_str += " "*2 + "name_override = \"" + omiclass + "\"\n"
+            rename_str += "\n[[processors.rename]]\n"
+            rename_str += " "*2 + "namepass = [\"" + plugin + "\"]\n"
 
             fields = ""
             ops_fields = ""
@@ -167,7 +115,7 @@ def parse_config(data):
                 #Add respective rename processor plugin based on the displayname
                 rename_str += "\n" + " "*2 + "[[processors.rename.replace]]\n" 
                 if "op" in telegraf_json[omiclass][plugin][field]:
-                    rename_str += " "*4 + "field = \"" + field + "_diff\"\n"
+                    rename_str += " "*4 + "field = \"" + field + "_rate\"\n"
                     rename_str += " "*4 + "dest = \"" + telegraf_json[omiclass][plugin][field]["displayName"] + "\"\n"
                 else:
                     rename_str += " "*4 + "field = \"" + field + "\"\n"
@@ -185,6 +133,11 @@ def parse_config(data):
             rename_str += "\n"
             input_str += " "*2 + "fieldpass = ["+fields[:-2]+"]\n"  #Using fields[: -2] here to get rid of the last ", " at the end of the string
             input_str += " "*2 + "interval = " + "\"" + min_interval + "\"\n\n"
+        
+            config_file["data"] = input_str + "\n" + rename_str + "\n" +aggregator_str
+
+            output.append(config_file)
+            config_file = {}
 
     """
     Sample telegraf TOML file output
@@ -213,9 +166,44 @@ def parse_config(data):
     period = "30s"
     drop_original = false
     fieldpass = ["Disk reads", "Disk writes", "Filesystem write bytes/sec"]
-    stats = ["diff"]
+    stats = ["rate"]
 
     """
 
-    telegraf_conf = input_str + "\n\n" + rename_str + "\n\n" +aggregator_str
-    return telegraf_conf, json.dumps(telegraf_json)
+    ## Get the log folder directory from HandlerEnvironment.json and use that for the telegraf default logging
+    logdir = "./LADtelegraf.log"
+    handler_env_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'HandlerEnvironment.json'))
+    print handler_env_path
+    if os.path.exists(handler_env_path):
+        with open(handler_env_path, 'r') as handler_env_file:
+            handler_env_txt = handler_env_file.read()
+        handler_env = json.loads(handler_env_txt)
+        if type(handler_env) == list:
+            handler_env = handler_env[0]
+        print handler_env
+        if "handlerEnvironment" in handler_env and "logFolder" in handler_env["handlerEnvironment"]:
+                logdir = handler_env["handlerEnvironment"]["logFolder"]
+    
+    # Telegraf basic agent and output config 
+    agentconf = "[agent]\n"
+    agentconf += "  interval = \"10s\"\n"
+    agentconf += "  round_interval = true"
+    agentconf += "  metric_batch_size = 1000\n"
+    agentconf += "  metric_buffer_limit = 10000\n"
+    agentconf += "  collection_jitter = \"0s\"\n"
+    agentconf += "  flush_interval = \"10s\"\n"
+    agentconf += "  flush_jitter = \"0s\"\n"
+    agentconf += "  logtarget = \"file\"\n"
+    agentconf += "  logfile = \"" + logdir + "/telegraf.log\n"
+    agentconf += "  logfile_rotation_max_size = \"100MB\"\n"
+    agentconf += "  logfile_rotation_max_archives = 5\n"                
+    agentconf += "\n# Configuration for sending metrics to InfluxD\n"
+    agentconf += "[[outputs.influxdb]]\n"
+
+    agent_file = {"filename":"telegraf.conf", "data": agentconf}
+    output.append(agent_file)
+
+
+    return output
+
+
---
+++ Diagnostic/Utils/telegraf_name_map.py
@@ -0,0 +1,66 @@
+name_map = { 
+
+######These are the counter keys and telegraf plugins for LAD
+
+"processor->CPU IO wait time" : {"plugin":"cpu", "field":"usage_iowait"},
+"processor->CPU user time" : {"plugin":"cpu", "field":"usage_user"},
+"processor->CPU nice time" : {"plugin":"cpu", "field":"usage_nice"},
+"processor->CPU percentage guest OS" : {"plugin":"cpu", "field":"usage_active"},
+"processor->CPU interrupt time" : {"plugin":"cpu", "field":"usage_irq"},
+"processor->CPU idle time" : {"plugin":"cpu", "field":"usage_idle"},
+"processor->CPU privileged time" : {"plugin":"cpu", "field":"usage_system"},
+
+"network->Network in guest OS" : {"plugin":"net", "field":"bytes_recv"},
+"network->Network total bytes" : {"plugin":"net", "field":"bytes_total"}, #Need to calculate sum
+"network->Network out guest OS" : {"plugin":"net", "field":"bytes_sent"},
+"network->Network collisions" : {"plugin":"net", "field":"drop_total"}, #Need to calculate sum
+"network->Packets received errors" : {"plugin":"net", "field":"err_in"},
+"network->Packets sent" : {"plugin":"net", "field":"packets_sent"},
+"network->Packets received" : {"plugin":"net", "field":"packets_recv"},
+"network->Packets sent errors" : {"plugin":"net", "field":"err_out"},
+
+"memory->Memory available" : {"plugin":"mem", "field":"available"},
+"memory->Mem. percent available" : {"plugin":"mem", "field":"available_percent"},
+"memory->Memory used" : {"plugin":"mem", "field":"used"},
+"memory->Memory percentage" : {"plugin":"mem", "field":"used_percent"}, 
+
+"memory->Swap available" : {"plugin":"swap", "field":"free"},
+"memory->Swap percent available" : {"plugin":"swap", "field":"free_percent"}, #Need to calculate percentage
+"memory->Swap used" : {"plugin":"swap", "field":"used"}, 
+"memory->Swap percent used" : {"plugin":"swap", "field":"used_percent"},
+
+"memory->Page reads": {"plugin":"kernel_vmstat", "field":"pgpgin", "op":"rate"},
+"memory->Page writes" : {"plugin":"kernel_vmstat", "field":"pgpgout", "op":"rate"},
+# "memory->Pages" : {"plugin":"kernel", "field":""},
+
+#OMI Filesystem plugin
+"filesystem->Filesystem used space" : {"plugin":"disk", "field":"used"},
+"filesystem->Filesystem % used space" : {"plugin":"disk", "field":"used_percent"},
+"filesystem->Filesystem free space" : {"plugin":"disk", "field":"free"},
+"filesystem->Filesystem % free space" : {"plugin":"disk", "field":"free_percent"}, #Need to calculate percentage
+"filesystem->Filesystem % free inodes" : {"plugin":"disk", "field":"inodes_free_percent"}, #Need to calculate percentage
+"filesystem->Filesystem % used inodes" : {"plugin":"disk", "field":"inodes_used_percent"}, #Need to calculate percentage
+
+# # "filesystem->Filesystem transfers/sec" : {"plugin":"diskio", "field":"reads + writes"}, #Need to calculate sum
+"filesystem->Filesystem read bytes/sec" : {"plugin":"diskio", "field":"read_bytes", "op":"rate"}, #Need to calculate rate (but each second not each interval)
+# # "filesystem->Filesystem bytes/sec" : {"plugin":"diskio", "field":"read_bytes + write_bytes"}, #Need to calculate rate and then sum
+"filesystem->Filesystem write bytes/sec" : {"plugin":"diskio", "field":"write_bytes", "op":"rate"}, #Need to calculate rate (but each second not each interval)
+"filesystem->Filesystem reads/sec" : {"plugin":"diskio", "field":"reads", "op":"rate"}, #Need to calculate rate (but each second not each interval)
+"filesystem->Filesystem writes/sec" : {"plugin":"diskio", "field":"writes", "op":"rate"}, #Need to calculate rate (but each second not each interval)
+
+# #OMI Disk plugin 
+"disk->Disk read guest OS" : {"plugin":"diskio", "field":"read_bytes", "op":"rate"},
+"disk->Disk write guest OS" : {"plugin":"diskio", "field":"write_bytes", "op":"rate"},
+# # "disk->Disk total bytes" : {"plugin":"diskio", "field":"read_bytes + write_bytes"},
+"disk->Disk reads" : {"plugin":"diskio", "field":"reads", "op":"rate"}, #Need to calculate rate (but each second not each interval)
+"disk->Disk writes" : {"plugin":"diskio", "field":"writes", "op":"rate"},
+# # "disk->Disk transfers" :
+"disk->Disk read time" : {"plugin":"diskio", "field":"read_time"},
+"disk->Disk write time" : {"plugin":"diskio", "field":"write_time"},
+"disk->Disk transfer time" :{"plugin":"diskio", "field":"io_time"},
+# # "disk->Disk queue length" : 
+
+
+##### These are the counter keys and telegraf plugins for Azure Monitor Agent
+
+}
