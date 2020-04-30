5835f35c2dd29d0c13e48f55b8526640d83a032b
Ivan Pavicevic <ivanp@microsoft.com>
2020-04-30 09:57:23 -0700

    TEMP: Testing reddog access during Windows phase.

---
+++ VMEncryption/cdpx_windows_restore.cmd
@@ -0,0 +1,3 @@
+echo "Trying access to reddog share START"
+dir /s \\reddog\Builds\branches\git_security_azurediskencryption_dev\1.0.4.1490\retail-amd64\EV2
+echo "Trying access to reddog share COMPLETE"
