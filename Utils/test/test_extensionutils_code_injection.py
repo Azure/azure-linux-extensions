e35a933da12976c7346da331083b4cf699554f15
bhbrahma <bhaskarbrahma@outlook.com>
2020-05-05 19:30:01 +0000

    updated test method

--- Utils/test/test_extensionutils_code_injection.py
+++ Utils/test/test_extensionutils_code_injection.py
@@ -28,16 +28,16 @@ class TestCodeInjection(unittest.TestCase):
     def test_code_injection(self):
         # failure cases
         exit_code, string_output = ext_utils.run_command_get_output("echo hello; echo world")
-        self.assertNotEquals(0, exit_code, "exit code != 0")
+        self.assertNotEqual(0, exit_code, "exit code != 0")
         exit_code, string_output = ext_utils.run_command_get_output(["echo hello; echo world"])
-        self.assertNotEquals(0, exit_code, "exit code != 0")
+        self.assertNotEqual(0, exit_code, "exit code != 0")
 
         # success case
         exit_code, string_output = ext_utils.run_command_get_output(["echo", "hello", ";", "echo", "world"])
-        self.assertEquals(0, exit_code, "exit code == 0")
-        self.assertEquals("hello ; echo world\n", string_output, "unexpected output")
+        self.assertEqual(0, exit_code, "exit code == 0")
+        self.assertEqual("hello ; echo world\n", string_output, "unexpected output")
         exit_code, string_output = ext_utils.run_command_get_output(["echo", "hello", "world"])
-        self.assertEquals(0, exit_code, "exit code == 0")
+        self.assertEqual(0, exit_code, "exit code == 0")
 
     def test_code_injection2(self):
         self.setup()
@@ -46,27 +46,27 @@ class TestCodeInjection(unittest.TestCase):
         out_file = self.get_random_filename()
         exit_code = ext_utils.run_command_and_write_stdout_to_file(
             "echo hello; echo world", out_file)
-        self.assertNotEquals(0, exit_code, "exit code != 0")
+        self.assertNotEqual(0, exit_code, "exit code != 0")
 
         out_file = self.get_random_filename()
         exit_code = ext_utils.run_command_and_write_stdout_to_file(
             ["echo hello; echo world"], out_file)
-        self.assertNotEquals(0, exit_code, "exit code != 0")
+        self.assertNotEqual(0, exit_code, "exit code != 0")
 
         # success case
         out_file = self.get_random_filename()
         exit_code = ext_utils.run_command_and_write_stdout_to_file(
             ["echo", "hello", ";", "echo", "world"], out_file)
-        self.assertEquals(0, exit_code, "exit code == 0")
+        self.assertEqual(0, exit_code, "exit code == 0")
         file_contents = ext_utils.get_file_contents(out_file)
-        self.assertEquals("hello ; echo world\n", file_contents, "unexpected output")
+        self.assertEqual("hello ; echo world\n", file_contents, "unexpected output")
 
         out_file = self.get_random_filename()
         exit_code = ext_utils.run_command_and_write_stdout_to_file([
             "echo", "hello", "world"], out_file)
-        self.assertEquals(0, exit_code, "exit code == 0")
+        self.assertEqual(0, exit_code, "exit code == 0")
         file_contents = ext_utils.get_file_contents(out_file)
-        self.assertEquals("hello world\n", file_contents, "unexpected output")
+        self.assertEqual("hello world\n", file_contents, "unexpected output")
 
 
 if __name__ == '__main__':
