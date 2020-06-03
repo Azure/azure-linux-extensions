f9f85f267a411e9194ec66494d173e3fd0c609ca
Henry Stolz <hestolz@microsoft.com>
2020-06-03 13:39:38 -0700

    1.13.13 versioning

--- OmsAgent/omsagent.py
+++ OmsAgent/omsagent.py
@@ -51,7 +51,7 @@ except Exception as e:
 ProceedOnSigningVerificationFailure = True
 PackagesDirectory = 'packages'
 keysDirectory = 'keys'
-BundleFileName = 'omsagent-1.13.5-0.universal.x64.sh'
+BundleFileName = 'omsagent-1.13.7-0.universal.x64.sh'
 GUIDRegex = r'[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}'
 GUIDOnlyRegex = r'^' + GUIDRegex + '$'
 SCOMCertIssuerRegex = r'^[\s]*Issuer:[\s]*CN=SCX-Certificate/title=SCX' + GUIDRegex + ', DC=.*$'
