9903b80ab09451e49b2fe31d72483434cfac9ace
Shrid <shrid.pant8@gmail.com>
2020-06-11 13:21:07 +0545

    Better debugging messages:

--- VMBackup/main/workloadPatch/WorkloadPatch.py
+++ VMBackup/main/workloadPatch/WorkloadPatch.py
@@ -39,7 +39,7 @@ class WorkloadPatch:
         self.enforce_slave_only = False
         self.role = "master"
         self.child = []
-        self.timeout = "800"
+        self.timeout = "300"
         self.confParser()
 
     def pre(self):
@@ -48,6 +48,7 @@ class WorkloadPatch:
             if self.role == "master" and int(self.enforce_slave_only) == 0:
                 if len(self.dbnames) == 0 :
                     #pre at server level create fork process for child and append
+                    self.logger.log("WorkloadPatch: Entered if conditions inside pre")
                     print("WorkloadPatch: Entered if conditions inside pre")
                     self.preMaster()
                 #else:
@@ -68,9 +69,11 @@ class WorkloadPatch:
 
     def post(self):
         try:
-            pre("WorkloadPatch: Entering workload post call")
+            self.logger.log("WorkloadPatch: Entering workload post call")
+            print("WorkloadPatch: Entering workload post call")
             if self.role == "master":
                 if len(self.dbnames) == 0:
+                    self.logger.log("WorkloadPatch: Entered if conditions inside post")
                     print("WorkloadPatch: Entered if conditions inside post")
                     #post at server level to turn off readonly mode
                     self.postMaster()
@@ -89,6 +92,7 @@ class WorkloadPatch:
             self.error_details.append("exception in processing of postscript")
 
     def preMaster(self):
+        self.logger.log("WorkloadPatch: Entering pre mode for master")
         print("WorkloadPatch: Entering pre mode for master")
         #if os.path.exists("/var/lib/mysql-files/azbackupserver.txt"):
         #    os.remove("/var/lib/mysql-files/azbackupserver.txt")
@@ -107,6 +111,7 @@ class WorkloadPatch:
             self.logger.log("WorkloadPatch: pre at server level completed")
         
         if self.name=='oracle':
+            self.logger.log("Shrid: Pre- Entering pre mode for master")
             print("Shrid: Pre- Entering pre mode for master")
             preOracle = "sqlplus -s / as sysdba @scripts/preOracleMaster.sql"
             args = ["su", "-", self.login_path, "-c", preOracle]
@@ -114,27 +119,32 @@ class WorkloadPatch:
             while process.poll() == None:
                 sleep(1)
             self.timeoutDaemon()
+            self.logger.log("Shrid: Pre- Exiting pre mode for master")
             print("Shrid: Pre- Exiting pre mode for master")
     
     def timeoutDaemon(self):
         global preDaemonThread
         if self.name=='oracle':
+            self.logger.log("Shrid: Inside oracle condition in timeout daemon")
             print("Shrid: Inside oracle condition in timeout daemon")
             preDaemonOracle = "sqlplus -s / as sysdba @scripts/preOracleDaemon.sql " + self.timeout
             argsDaemon = ["su", "-", self.login_path, "-c", preDaemonOracle]
             preDaemonThread = threading.Thread(target=self.threadForPreDaemon, args=[argsDaemon])
             preDaemonThread.start()
+        self.logger.log("Shrid: timeoutDaemon started for: " + self.timeout + " seconds")
         print("Shrid: timeoutDaemon started for: ", self.timeout, " seconds")
 
     def threadForPreDaemon(self, args): 
             global daemonProcess
             daemonProcess = subprocess.Popen(args)
-            print("Shrid: darmonProcess started")
+            self.logger.log("Shrid: daemonProcess started")
+            print("Shrid: daemonProcess started")
             while daemonProcess.poll() == None:
                 sleep(1)
+            self.logger.log("Shrid: daemonProcess completed")
             print("Shrid: daemonProcess completed")
 
-    def thread_for_sql(self,args):s
+    def thread_for_sql(self,args):
         self.logger.log("command to execute: "+str(args))
         self.child.append(subprocess.Popen(args,stdout=subprocess.PIPE,stdin=subprocess.PIPE,shell=True,stderr=subprocess.PIPE))
         while len(self.child) == 0:
@@ -159,6 +169,7 @@ class WorkloadPatch:
         pass
     
     def postMaster(self):
+        self.logger.log("WorkloadPatch: Entering post mode for master")
         print("WorkloadPatch: Entering post mode for master")
         #if os.path.exists("/var/lib/mysql-files/azbackupserver.txt"):
         #    os.remove("/var/lib/mysql-files/azbackupserver.txt")
@@ -177,12 +188,16 @@ class WorkloadPatch:
             post_child = subprocess.Popen(args,stdout=subprocess.PIPE,stdin=subprocess.PIPE,shell=True,stderr=subprocess.PIPE)
 
         if 'oracle' in self.name.lower():
+            self.logger.log("Shird: Post- Inside try")
             print("Shird: Post- Inside try")
             if preDaemonThread.isAlive():
+                self.logger.log("Shird: Post- Backup successful")
                 print("Shird: Post- Backup successful")
+                self.logger.log("Shrid: Post- Initiating Post Script")
                 print("Shrid: Post- Initiating Post Script")
                 daemonProcess.terminate()
             else:
+                self.logger.log("Shrid: Post- Backup unsuccessful")
                 print("Shrid: Post- Backup unsuccessful")
                 return
             postOracle="sqlplus -s / as sysdba @scripts/postOracleMaster.sql"
@@ -190,6 +205,7 @@ class WorkloadPatch:
             process = subprocess.Popen(args)
             while process.poll()==None:
                 sleep(1)
+            self.logger.log("Shrid: Post- Completed")
             print("Shrid: Post- Completed")
 
 
