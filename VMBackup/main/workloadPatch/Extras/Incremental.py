783a372bf9df4826ddbb943886206f483448b979
Shrid <shrid.pant8@gmail.com>
2020-06-17 11:29:31 +0545

    Changed to classes

--- VMBackup/main/workloadPatch/Extras/Incremental.py
+++ VMBackup/main/workloadPatch/Extras/Incremental.py
@@ -1,138 +1,142 @@
 import sys
-import Utils.HandlerUtil
 import threading
 import os
 from time import sleep
+import subprocess
+from datetime import datetime
+import re
+#import Utils.HandlerUtil
 try:
     import ConfigParser as ConfigParsers
 except ImportError:
     import configparser as ConfigParsers
-import subprocess
-from datetime import datetime
-import re
 
-#----Config----#
-name = "oracle"
-login_path = "oracle"
-backupSource = ""
-baseLocation = "/hdd/AutoIncrement/"
-parameterFilePath = "/u01/app/oracle/product/19.3.0/dbhome_1/dbs/initCDB1.ora"
-oracleParameter = {}
-parameterFileParser()
-#Action = 'b'
+class Incremental:
+    def __init__(self):
+        self.name = "oracle"
+        self.login_path = "oracle"
+        self.baseLocation = "/hdd/AutoIncrement/"
+        self.parameterFilePath = "/u01/app/oracle/product/19.3.0/dbhome_1/dbs/initCDB1.ora"
+        self.oracleParameter = {}
+        self.backupSource = ""
+        #Action = 'b' #To always perform backup by default
+
+    def parameterFileParser(self):
+        regX = re.compile(r"\*\..+=.+")
+        parameterFile = open(self.parameterFilePath, 'r')
+        contents = parameterFile.read()
+        for match in regX.finditer(contents):
+            keyParameter = match.group().split('=')[0].lstrip('*\.')
+            valueParameter = [name.strip('\'') for name in match.group().split('=')[1].split(',')]
+            self.oracleParameter[keyParameter] = valueParameter
+        #print(self.oracleParameter)
+
+    def setLocation(self):
+        nowTimestamp = datetime.now()
+        nowTimestamp = nowTimestamp.strftime("%Y%m%d%H%M%S")
+        fullPath = self.baseLocation + nowTimestamp
+        os.system('mkdir -m777 '+ fullPath)
+        return fullPath
+
+    #----Start Incremental Backup----#
+    def incremental(self):
+        print("Incremental: Taking a backup")
+
+        backupPath = self.setLocation()
+
+        if 'oracle' in self.name.lower():
+            backupOracle = "sqlplus -s / as sysdba @/hdd/python/IncrementalBackup/backup.sql " + backupPath
+            argsForControlFile = ["su", "-", self.login_path, "-c", backupOracle]
+            snapshotControlFile = subprocess.Popen(argsForControlFile)
+            while snapshotControlFile.poll()==None:
+                sleep(1)        
+            recoveryFileDest = self.oracleParameter['db_recovery_file_dest']
+            dbName = self.oracleParameter['db_name']
+            print('Archive log started: ', datetime.now().strftime("%Y%m%d%H%M%S"))
+            os.system('cp -R -f ' + recoveryFileDest[0] + '/' + dbName[0] + '/archivelog ' + backupPath)
+            print('Archive log copied: ', datetime.now().strftime("%Y%m%d%H%M%S"))
+
+        print("Incremental: Backup Complete")
+    #----End Incremental Backup----#
+
+    #----Start Incremental Restore----#
+    def switchControlFiles(self, backupPath):
+        parsedControlFile = self.oracleParameter['control_files']
+        for location in parsedControlFile:
+            os.system('rm -f '+location)
+            os.system('cp -f '+ backupPath + '/control.ctl ' + location)
+            os.system('chmod a+wrx '+location)
+        print("Switched Control Files")
+
+    def switchArchiveLogFiles(self, backupPath):
+        recoveryFileDest = self.oracleParameter['db_recovery_file_dest']
+        dbName = self.oracleParameter['db_name']
+        for location in recoveryFileDest:
+            os.system('rm -R -f '+ location + '/' + dbName[0] +'/archivelog')
+            os.system('cp -R -f ' + backupPath + '/archivelog ' + location + '/' + dbName[0] + '/archivelog')
+            os.system('chmod -R a+wrx '+ location +'/' + dbName[0] + '/archivelog')
+        print("Switched Archive Log Files")
+
+    def restore(self):
+        print("Incremental: Restoring")
+        backupPath = self.baseLocation + self.backupSource
+        scriptLocation = "/hdd/python/IncrementalBackup/recover.sh"
+        args = [scriptLocation, self.backupSource, self.login_path]
+        restoreProcess = subprocess.Popen(args)
+        while restoreProcess.poll()==None:
+            sleep(2)
+        self.switchControlFiles(backupPath)
+        self.switchArchiveLogFiles(backupPath)
+        print("Incremental: Restore Complete")
+    #----End Incremental Restore----#
+
+    def confParser(self):
+        print("WorkloadPatch: Entering workload config parsing")
+        configfile = 'increment.conf'
+        if os.path.exists(configfile):
+            config = ConfigParsers.ConfigParser()
+            config.read(configfile)
+            if config.has_section("incremental"):
+                print("Incremental: config section present for incremental ")
+                if config.has_option("incremental", 'workload_name'):                        
+                    name = config.get("incremental", 'workload_name')
+                    print("Incremental: config incremental command ", name)
+                else:
+                    return None
+                if config.has_option("incremental", 'loginPath'):
+                    login_path = config.get("incremental", 'loginPath')
+                    print("Incremental: config incremental login_path ", login_path)
+                if config.has_option("incremental", 'parameterFilePath'):
+                    parameterFilePath = config.get("incremental", 'parameterFilePath')
+                    print("Incremental: config incremental parameterFilePath ", parameterFilePath)
+                if config.has_option("incremental", 'baseLocation'):
+                    baseLocation = config.get("incremental", 'baseLocation')
+                    print("Incremental: config incremental baseLocation ", baseLocation)
+                if config.has_option("incremental", 'backupSource'):
+                    backupSource = config.get("incremental", 'backupSource')
+                    print("Incremental: config incremental backupSource ", backupSource)
+        else:
+            print("No matching workload config found")
+
+oracleIncremental = Incremental()
+#----Parse the parameter file----#
+oracleIncremental.parameterFileParser()
 #----End----#
-
-def confParser():
-    print("WorkloadPatch: Entering workload config parsing")
-    configfile = 'increment.conf'
-    if os.path.exists(configfile):
-        config = ConfigParsers.ConfigParser()
-        config.read(configfile)
-        if config.has_section("incremental"):
-            print("Incremental: config section present for incremental ")
-            if config.has_option("incremental", 'workload_name'):                        
-                name = config.get("incremental", 'workload_name')
-                print("Incremental: config incremental command ", name)
-            else:
-                return None
-            if config.has_option("incremental", 'loginPath'):
-                login_path = config.get("incremental", 'loginPath')
-                print("Incremental: config incremental login_path ", login_path)
-            if config.has_option("incremental", 'parameterFilePath'):
-                parameterFilePath = config.get("incremental", 'parameterFilePath')
-                print("Incremental: config incremental parameterFilePath ", parameterFilePath)
-            if config.has_option("incremental", 'baseLocation'):
-                baseLocation = config.get("incremental", 'baseLocation')
-                print("Incremental: config incremental baseLocation ", baseLocation)
-            if config.has_option("incremental", 'backupSource'):
-                backupSource = config.get("incremental", 'backupSource')
-                print("Incremental: config incremental backupSource ", backupSource)
-    else:
-        print("No matching workload config found")
-
-def parameterFileParser():
-    regX = re.compile(r"\*\..+=.+")
-    parameterFile = open(parameterFilePath, 'r')
-    contents = parameterFile.read()
-    for match in regX.finditer(contents):
-        keyParameter = match.group().split('=')[0].lstrip('*\.')
-        valueParameter = [name.strip('\'') for name in match.group().split('=')[1].split(',')]
-        oracleParameter[keyParameter] = valueParameter
-    #print(oracleParameter)
-
-def setLocation():
-    nowTimestamp = datetime.now()
-    nowTimestamp = nowTimestamp.strftime("%Y%m%d%H%M%S")
-    fullPath = baseLocation + nowTimestamp
-    os.system('mkdir -m777 '+ fullPath)
-    return fullPath
-
-#----Start Incremental Backup----#
-def incremental():
-    print("Incremental: Taking a backup")
-
-    backupPath = setLocation()
-
-    if 'oracle' in name.lower():
-        backupOracle = "sqlplus -s / as sysdba @/hdd/python/IncrementalBackup/backup.sql " + backupPath
-        argsForControlFile = ["su", "-", login_path, "-c", backupOracle]
-        snapshotControlFile = subprocess.Popen(argsForControlFile)
-        while snapshotControlFile.poll()==None:
-            sleep(1)        
-        recoveryFileDest = oracleParameter['db_recovery_file_dest']
-        dbName = oracleParameter['db_name']
-        print('Archive log started: ', datetime.now().strftime("%Y%m%d%H%M%S"))
-        os.system('cp -R -f ' + recoveryFileDest[0] + '/' + dbName[0] + '/archivelog ' + backupPath)
-        print('Archive log copied: ', datetime.now().strftime("%Y%m%d%H%M%S"))
-
-    print("Incremental: Backup Complete")
-#----End Incremental Backup----#
-
-#----Start Incremental Restore----#
-def switchControlFiles(backupPath):
-    parsedControlFile = oracleParameter['control_files']
-    for location in parsedControlFile:
-        os.system('rm -f '+location)
-        os.system('cp -f '+ backupPath + '/control.ctl ' + location)
-        os.system('chmod a+wrx '+location)
-    print("Switched Control Files")
-
-def switchArchiveLogFiles(backupPath):
-    recoveryFileDest = oracleParameter['db_recovery_file_dest']
-    dbName = oracleParameter['db_name']
-    for location in recoveryFileDest:
-        os.system('rm -R -f '+ location + '/' + dbName[0] +'/archivelog')
-        os.system('cp -R -f ' + backupPath + '/archivelog ' + location + '/' + dbName[0] + '/archivelog')
-        os.system('chmod -R a+wrx '+ location +'/' + dbName[0] + '/archivelog')
-    print("Switched Archive Log Files")
-
-def restore():
-    print("Incremental: Restoring")
-    backupPath = baseLocation + backupSource
-    scriptLocation = "/hdd/python/IncrementalBackup/recover.sh"
-    args = [scriptLocation, backupSource, login_path]
-    restoreProcess = subprocess.Popen(args)
-    while restoreProcess.poll()==None:
-        sleep(2)
-    switchControlFiles(backupPath)
-    switchArchiveLogFiles(backupPath)
-    print("Incremental: Restore Complete")
-#----End Incremental Restore----#
 
 #----Prompt----#
 Action = input("Enter l for list, b for incremental backup and r for restore: ")
 #----End Prompt----#
 
 if Action=='b':
-    incremental()
+    oracleIncremental.incremental()
 elif Action=='r':
     #----Restore----#
-    os.system('ls -lrt '+ baseLocation)
-    backupSource = input("Enter the filename: ")
-    restore()
+    os.system('ls -lrt '+ oracleIncremental.baseLocation)
+    oracleIncremental.backupSource = input("Enter the filename: ")
+    oracleIncremental.restore()
     #----End Restore----#
 elif Action=='l':
-    os.system('ls -lrt '+ baseLocation)
+    os.system('ls -lrt '+ oracleIncremental.baseLocation)
 else:
     print("Invalid input")
 
--- VMBackup/main/workloadPatch/Extras/OracleUtility.py
+++ VMBackup/main/workloadPatch/Extras/OracleUtility.py
@@ -21,41 +21,38 @@ import Utils.HandlerUtil
 import threading
 import os
 from time import sleep
+import subprocess
 try:
     import ConfigParser as ConfigParsers
 except ImportError:
     import configparser as ConfigParsers
-import subprocess
 
 class OracleUtility:
     def __init__(self, logger):
         self.logger = logger
-        self.name = "oracle"
         self.dbnames = []
         self.login_path = "oracle"
 
     def containerName(self):
-
-            if 'oracle' in self.name.lower():
-                containerNameArgs =  "su - " + self.login_path + " -c " + "'sqlplus -s / as sysdba<<-EOF\nSHOW CON_NAME;\nEOF'"
-                oracleContainerName = subprocess.check_output(containerNameArgs, shell=True)
-                self.logger.log("Shrid: containerName- " + str(oracleContainerName))
-                print("Shrid: containerName- ", str(oracleContainerName))
-                
-                if "CDB$ROOT" in str(oracleContainerName):
-                    self.logger.log("Shrid: containerName- In CDB$ROOT")
-                    print("Shrid: containerName- In CDB$ROOT")
-                    return True
-                else:
-                    self.logger.log("Shrid: Pre- Error. Not in CDB$ROOT")
-                    print("Shrid: Pre- Error. Not in CDB$ROOT")
-                    changeContainerArgs = "su - " + self.login_path + " -c " + "'sqlplus -s / as sysdba<<-EOF\nALTER SESSION SET CONTAINER=CDB$ROOT;\nEOF'"
-                    oracleChangeContainer = subprocess.check_output(changeContainerArgs, shell=True)
-                    self.logger.log("Shrid: containerName- " + str(oracleChangeContainer))
-                    print("Shrid: containerName- ", str(oracleChangeContainer))
-                    if "Session altered." in str(oracleChangeContainer):
-                        return True
-                    else:
-                        return False
-            
-            return False
+        containerNameArgs =  "su - " + self.login_path + " -c " + "'sqlplus -s / as sysdba<<-EOF\nSHOW CON_NAME;\nEOF'"
+        oracleContainerName = subprocess.check_output(containerNameArgs, shell=True)
+        self.logger.log("Shrid: containerName- " + str(oracleContainerName))
+        print("Shrid: containerName- ", str(oracleContainerName))
+        
+        if "CDB$ROOT" in str(oracleContainerName):
+            self.logger.log("Shrid: containerName- In CDB$ROOT")
+            print("Shrid: containerName- In CDB$ROOT")
+            return True
+        else:
+            self.logger.log("Shrid: Pre- Error. Not in CDB$ROOT")
+            print("Shrid: Pre- Error. Not in CDB$ROOT")
+            changeContainerArgs = "su - " + self.login_path + " -c " + "'sqlplus -s / as sysdba<<-EOF\nALTER SESSION SET CONTAINER=CDB$ROOT;\nEOF'"
+            oracleChangeContainer = subprocess.check_output(changeContainerArgs, shell=True)
+            self.logger.log("Shrid: containerName- " + str(oracleChangeContainer))
+            print("Shrid: containerName- ", str(oracleChangeContainer))
+            if "Session altered." in str(oracleChangeContainer):
+                return True
+            else:
+                return False
+    
+        return False
