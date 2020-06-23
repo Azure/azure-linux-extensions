c62eef9511f05f2c7cb27295881c93db417f3cfc
Shrid <shrid.pant8@gmail.com>
2020-06-23 12:36:17 +0545

    new edits

--- VMBackup/main/test_WorkloadPatch.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import workloadPatch.WorkloadPatch as wp
-
-class TestWP(unittest.TestCase):
-    def test_preMaster(self):
-        self.assertEquals(wp.preMaster(), None)
-
-if __name__ == '__main__':
-    unittest.main()
