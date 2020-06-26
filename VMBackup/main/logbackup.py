e1935925d2f71a26ba8fca613c81bb3136270332
Shrid <shrid.pant8@gmail.com>
2020-06-26 16:10:46 +0545

    Reorganization

--- VMBackup/main/logbackup.py
+++ VMBackup/main/logbackup.py
@@ -30,7 +30,7 @@ def takeBackup():
     backupPath = setLocation()
 
     if 'oracle' in logbackup.name.lower():
-        backupOracle = "sqlplus -s / as sysdba @" +  "/var/lib/waagent/Microsoft.Azure.RecoveryServices.VMSnapshotLinux-1.0.9164.0/main/workloadPatch/scripts/logbackup.sql " + backupPath
+        backupOracle = logbackup.command + " -s / as sysdba @" +  "/var/lib/waagent/Microsoft.Azure.RecoveryServices.VMSnapshotLinux-1.0.9164.0/main/workloadPatch/scripts/logbackup.sql " + backupPath
         argsForControlFile = ["su", "-", logbackup.cred_string, "-c", backupOracle]
         snapshotControlFile = subprocess.Popen(argsForControlFile)
         while snapshotControlFile.poll()==None:
