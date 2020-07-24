3632f3561ea8f98f611d91dad7ddcceef3eb6d96
Henry Stolz <43051891+hestolz@users.noreply.github.com>
2020-07-24 16:03:54 -0700

    improve packaging script (#1173)

--- OmsAgent/packaging.sh
+++ OmsAgent/packaging.sh
@@ -5,18 +5,18 @@ source omsagent.version
 usage()
 {
     local basename=`basename $0`
-    echo
-    echo "$basename <PATH_TO_OMSAGENT_SHELL_BUNDLE>"
+    echo "usage: ./$basename <path to omsagent-<version>.universal.x64{.sh, .sha256sums, .asc}> [path for zip output]"
 }
 
-bundle_path=$1
+input_path=$1
 output_path=$2
 PACKAGE_NAME="oms$OMS_EXTENSION_VERSION.zip"
 if [[ "$1" == "--help" ]]; then
     usage
     exit 0
-elif [[ ! -f $bundle_path ]]; then
-    echo "OMS bundle '$bundle_path' not found"
+elif [[ ! -d $input_path ]]; then
+    echo "OMS files path '$input_path' not found"
+    usage
     exit 1
 fi
 
@@ -32,7 +32,7 @@ cp ../Common/WALinuxAgent-2.0.16/waagent .
 rm -rf packages ext/future
 mkdir -p packages ext/future
 # copy shell bundle to packages/
-cp $bundle_path packages/
+cp $input_path/omsagent-$OMS_SHELL_BUNDLE_VERSION.universal.x64.* packages/
 # copy just the source of python-future
 cp -r ext/python-future/src/* ext/future
 # sync the file copy
