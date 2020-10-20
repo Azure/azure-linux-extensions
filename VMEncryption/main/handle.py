780fd69b14e0e152348794775ebf452f378a7673
SHRIDHR PARAMESHWAR BHAT <shparame@microsoft.com>
2020-10-20 11:07:52 -0700

    Adding Resource disk validation for encryptFormatAll (#1225)
    * Adding Resource disk validation for encryptFormatAll
    * Moving the resource disk check to encrypt_format_mount() function and adding unit tests
    * Correcting the typo in the error message
    * Updating logging for readability

--- VMEncryption/main/handle.py
+++ VMEncryption/main/handle.py
@@ -927,8 +927,7 @@ def enable_encryption():
                                           code=str(CommonVariables.configuration_error),
                                           message='Failed to encrypt resource disk. Please make sure no process is using it.')
                         else:
-                            logger.log("Resource disk encrypted successfully.")
-
+                            logger.log("Resource Disk is either absent or encrypted successfully.")
                 encryption_marker = mark_encryption(command=extension_parameter.command,
                                                     volume_type=extension_parameter.VolumeType,
                                                     disk_format_query=extension_parameter.DiskFormatQuery)
