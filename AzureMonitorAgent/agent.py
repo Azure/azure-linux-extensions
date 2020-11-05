5ad9246a7dca648e76ed77297e10bb5e56daab4e
Nikhil Dhanda <nidhanda@microsoft.com>
2020-11-05 13:58:32 -0800

    Fixing command run and logging error

--- AzureMonitorAgent/agent.py
+++ AzureMonitorAgent/agent.py
@@ -972,7 +972,7 @@ def run_command_and_log(cmd, check_error = True, log_cmd = True):
     """
     exit_code, output = run_get_output(cmd, check_error, log_cmd)
     if log_cmd:
-        hutil_log_info('Output of command "{0}": \n{1}'.format(cmd, output))
+        hutil_log_info('Output of command "{0}": \n{1}'.format(cmd.rstrip(), output))
     else:
         hutil_log_info('Output: \n{0}'.format(output))
         
@@ -1276,8 +1276,14 @@ def run_get_output(cmd, chk_err = False, log_cmd = True):
         except subprocess.CalledProcessError as e:
             exit_code = e.returncode
             output = e.output
+    
+    output = output.encode('utf-8')
 
-    return exit_code, output.encode('utf-8').strip()
+    # On python 3, encode returns a byte object, so we must decode back to a string
+    if sys.version_info >= (3,):
+        output = output.decode()
+
+    return exit_code, output.strip()
 
 
 def init_waagent_logger():
