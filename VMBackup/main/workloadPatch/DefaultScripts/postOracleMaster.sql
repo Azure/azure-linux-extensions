aee577b1e344cdc9e22c4bcdff27f02183165d82
soumyapattnaik <33341338+soumyapattnaik@users.noreply.github.com>
2020-12-16 04:27:07 -0800

    Enhancements to pre post script for oracle (#1234)
    * skip mount snapshot
    
    * changes for unable to sleep
    
    * code review comments
    
    * private fix
    
    * workload running changes
    
    * workload running changes
    
    * fifreeze logging
    
    * fifreeze logging
    
    * fifreeze logging
    
    * code review changes
    
    * fix for python 3.8
    
    * more changes
    
    * more changes
    
    * changes for python3.8
    
    * changes for python3.8
    
    * changes for python3.8
    
    * version change for release
    
    * version change for release
    
    * setup python changes
    
    * changes for error
    
    * changes for error
    
    * changes for error
    
    * deployment version change
    
    * setup py changes
    
    * bug fixes
    
    * fixes for oracle backup
    
    * changes based on PM feedback
    
    * unneccessary import removed
    
    * remove daemon thread unwanted code
    
    * deployment
    
    * base checkin for postgres
    
    * changes for postgres
    
    * Update postMasterOracle.sql
    
    1) Remove use of SUSPEND, which is dependent on seldom-used Oracle Resource Manager
    2) Handle scenario with SQL*Plus connecting "/NOLOG" and database instance not OPEN successfully
    3) Handle scenario with database instance running in NOARCHIVELOG mode successfully
    
    * Update preOracleMaster.sql
    
    1) remove use of SUSPEND, which is dependent on seldom-used Oracle Resource Manager
    2) handle scenario where SQL*Plus is started with "/NOLOG" option and Oracle database instance is not OPEN
    3) handle scenario where Oracle database instance is running NOARCHIVELOG mode
    Goal:  perform ARCHIVE LOG CURRENT (in ARCHIVELOG mode) or LOGFILE SWITCH (in NOARCHIVELOG mode) in order to force a full database checkpoint, then put entire database into BACKUP mode
    
    * Update WorkloadPatch.py
    
    1) continue to call Oracle SQL*Plus with "-S" flag to suppress banners, prompts, etc
    2) Call Oracle SQL*Plus with "-R 2" flag to restrict use of SQL*Plus access to underlying operating system.  For example, permit use of the START (or "@") command to run SQL scripts, but remove use of HOST or SPOOL commands
    3) Change call to Oracle SQL*Plus from forcing connection to database when called, to instead starting program without forcing connection to database, using "/NOLOG" switch instead of database credentials.
    NOTE:  the "preOracleMaster.sql" and "postOracleMaster.sql" scripts called by SQL*Plus will connect to the database as the limited SYSBACKUP role using external OS authentication instead of connecting to the database the all-powerful SYSDBA role using external authentication
    
    * changes for postgres
    
    * changes for postgres
    
    * changes for postgres
    
    * Update PostOracleMaster.sql
    
    Correct typo from mention of "pre-script" to "post-script" in "azmessage" calls
    
    * changes for telemetry
    
    * changes for logging
    
    * oracle error handling
    
    * changes for deployment
    
    * code review comments implemented
    
    * code review comments implemented
    
    * Update preOracleMaster.sql
    
    Add query for LOG_MODE from V$DATABASE
    
    * Update postOracleMaster.sql
    
    Add query for LOG_MODE from V$DATABASE
    
    * Update preOracleMaster.sql
    
    Make standard output more terse, with display of STATUS and LOG_MODE on a single-line for easier parsing
    
    * Update postOracleMaster.sql
    
    Make standard output more terse, with display of STATUS and LOG_MODE on a single-line for easier parsing
    
    * pre post ora changes
    
    * changes for 1912
    
    * changes for oracle
    
    * changes for oracle
    
    * updated xml
    
    Co-authored-by: Tim Gorman <59261805+tigormanmsft@users.noreply.github.com>

--- VMBackup/main/workloadPatch/DefaultScripts/postOracleMaster.sql
+++ VMBackup/main/workloadPatch/DefaultScripts/postOracleMaster.sql
@@ -1,31 +1,23 @@
 REM ================================================================================
-REM File:	postOracleMaster.sql
-REM Date:	16-Sep 2020
-REM Type:	Oracle SQL*Plus script
-REM Author:	Microsoft CAE team
+REM File:       postOracleMaster.sql
+REM Date:       16-Sep 2020
+REM Type:       Oracle SQL*Plus script
+REM Author:     Microsoft CAE team
 REM
 REM Description:
-REM		Oracle SQL*Plus script called as an Azure Backup "post" script to
-REM		run immediately following a backup snapshot.
+REM             Oracle SQL*Plus script called as an Azure Backup "post" script to
+REM             run immediately following a backup snapshot.
 REM
-REM		SQL*Plus is executed in RESTRICTED LEVEL 2 mode, which means that
-REM		commands like HOST and SPOOL are not permitted, but commands like
-REM		START are permitted.
+REM             SQL*Plus is executed in RESTRICTED LEVEL 2 mode, which means that
+REM             commands like HOST and SPOOL are not permitted, but commands like
+REM             START are permitted.
 REM ================================================================================
 REM
 REM ********************************************************************************
-REM Connect this SQL*Plus session to the current database instance as SYSBACKUP...
-REM (be sure to leave one blank line before the CONNECT command)
+REM Format standard output to be terse...
 REM ********************************************************************************
 
-CONNECT / AS SYSBACKUP
-
-REM
-REM ********************************************************************************
-REM Emit messages to standard output using DBMS_OUTPUT package...
-REM ********************************************************************************
-
-SET ECHO OFF FEEDBACK OFF TIMING OFF SERVEROUTPUT ON PAGESIZE 100 LINESIZE 130
+SET ECHO OFF FEEDBACK OFF TIMING OFF PAGESIZE 0 LINESIZE 130 TRIMOUT ON TRIMSPOOL ON
 
 REM
 REM ********************************************************************************
@@ -37,6 +29,14 @@ REM SET ECHO ON FEEDBACK ON TIMING ON
 
 REM
 REM ********************************************************************************
+REM Connect this SQL*Plus session to the current database instance as SYSBACKUP...
+REM (be sure to leave one blank line before the CONNECT command)
+REM ********************************************************************************
+
+CONNECT / AS SYSBACKUP
+
+REM
+REM ********************************************************************************
 REM Retrieve the status of the Oracle database instance, and exit from SQL*Plus
 REM with SUCCESS exit status if database instance is not OPEN...
 REM ********************************************************************************
@@ -44,8 +44,8 @@ REM ****************************************************************************
 WHENEVER OSERROR EXIT SUCCESS
 WHENEVER SQLERROR EXIT SUCCESS
 COL STATUS NEW_VALUE V_STATUS
-SELECT STATUS FROM V$INSTANCE;
-EXEC IF '&&V_STATUS' <> 'OPEN' THEN RAISE NOT_LOGGED_ON; END IF;
+SELECT 'STATUS='||STATUS AS STATUS FROM V$INSTANCE;
+EXEC IF '&&V_STATUS' <> 'STATUS=OPEN' THEN RAISE NOT_LOGGED_ON; END IF;
 
 REM
 REM ********************************************************************************
@@ -56,41 +56,55 @@ REM ****************************************************************************
 WHENEVER OSERROR EXIT FAILURE
 WHENEVER SQLERROR EXIT FAILURE
 
-REM 
+REM
+REM ********************************************************************************
+REM Display the LOG_MODE of the database to be captured by the calling Python code...
+REM ********************************************************************************
+
+SELECT 'LOG_MODE='||LOG_MODE AS LOG_MODE FROM V$DATABASE;
+
+REM
+REM ********************************************************************************
+REM Enable emitting DBMS_OUTPUT to standard output...
+REM ********************************************************************************
+
+SET SERVEROUTPUT ON SIZE 1000000
+
+REM
 REM ********************************************************************************
 REM Attempt to put the database into BACKUP mode, which will succeed only if the
 REM database is presently in ARCHIVELOG mode
 REM ********************************************************************************
 
 DECLARE
-	v_errcontext	varchar2(128);
-	noArchiveLog	exception;
-	notInBackup	exception;
-	pragma 		exception_init(noArchiveLog, -1123);
-	pragma 		exception_init(notInBackup, -1142);
+        v_errcontext    varchar2(128);
+        noArchiveLog    exception;
+        notInBackup     exception;
+        pragma          exception_init(noArchiveLog, -1123);
+        pragma          exception_init(notInBackup, -1142);
 BEGIN
-	--
-	v_errcontext := 'azmessage-1';
-	sysbackup.azmessage('INFO - AzBackup post-script: END BACKUP starting...');
-	--
-	v_errcontext := 'END BACKUP';
-	execute immediate 'ALTER DATABASE END BACKUP';
-	--
-	v_errcontext := 'azmessage-2';
-	sysbackup.azmessage('INFO - AzBackup post-script: END BACKUP succeeded');
-	--
+        --
+        v_errcontext := 'azmessage-1';
+        sysbackup.azmessage('INFO - AzBackup post-script: END BACKUP starting...');
+        --
+        v_errcontext := 'END BACKUP';
+        execute immediate 'ALTER DATABASE END BACKUP';
+        --
+        v_errcontext := 'azmessage-2';
+        sysbackup.azmessage('INFO - AzBackup post-script: END BACKUP succeeded');
+        --
 EXCEPTION
-	when noArchiveLog then
-		sysbackup.azmessage('INFO - AzBackup post-script: END BACKUP in NOARCHIVELOG failed, no action required');
-	when notInBackup then
-		sysbackup.azmessage('WARN - AzBackup post-script: END BACKUP failed - no datafiles in backup');
-	when others then
-		sysbackup.azmessage('FAIL - AzBackup post-script: ' || v_errcontext || ' failed');
-		raise;
+        when noArchiveLog then
+                sysbackup.azmessage('INFO - AzBackup post-script: END BACKUP in NOARCHIVELOG failed, no action required');
+        when notInBackup then
+                sysbackup.azmessage('WARN - AzBackup post-script: END BACKUP failed - no datafiles in backup');
+        when others then
+                sysbackup.azmessage('FAIL - AzBackup post-script: ' || v_errcontext || ' failed');
+                raise;
 END;
 /
 
-REM 
+REM
 REM ********************************************************************************
 REM Exit from Oracle SQL*Plus with SUCCESS exit status...
 REM ********************************************************************************
