3cacb9b9a76084cec0d0dc209cb7e7217f53ee6d
Eric Jarvi <ejarvi@users.noreply.github.com>
2021-06-09 19:09:27 -0700

    check for empty string

--- VMEncryption/main/ResourceDiskUtil.py
+++ VMEncryption/main/ResourceDiskUtil.py
@@ -86,11 +86,13 @@ class ResourceDiskUtil(object):
         return (int)(self.executor.Execute(cmd, suppress_logging=True)) == CommonVariables.process_success
 
     def _resource_disk_exists(self):
-        """ true if udev name for resource disk exists and is mounted """
-        cmd = 'mountpoint -q ' + self._get_rd_base_dev_path()
-        if (int)(self.executor.Execute(cmd, suppress_logging=True)) == CommonVariables.process_success:
-            self.logger.log("Resource disk exists.")
-            return True
+        """ true if udev name for resource disk exists """
+        rd_base_dev_path = self._get_rd_base_dev_path()
+        if rd_base_dev_path:
+            cmd = 'test -b' + self._get_rd_base_dev_path()
+            if (int)(self.executor.Execute(cmd, suppress_logging=True)) == CommonVariables.process_success:
+                self.logger.log("Resource disk exists.")
+                return True
         self.logger.log("Resource disk does not exist.")
         return False
 
