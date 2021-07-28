af2eacda5f8d25bb840cf97057db50033d3c76e6
Vishal Mishra <vimish@microsoft.com>
2021-07-28 13:28:22 -0700

    Add BEK disk validations.

--- VMEncryption/main/BekUtil.py
+++ VMEncryption/main/BekUtil.py
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from Common import TestHooks
+from Common import TestHooks, CommonVariables
 import base64
 import os.path
 import traceback
@@ -25,7 +25,13 @@ import sys
 """
 add retry-logic to the network api call.
 """
+class BekMissingException(Exception):
 
+   def __init__(self, value):
+      self.value = value
+
+   def __str__(self):
+      return(repr(self.value))
 
 class BekUtil(object):
     """
@@ -36,6 +42,11 @@ class BekUtil(object):
         self.logger = logger
         self.bek_filesystem_mount_point = '/mnt/azure_bek_disk'
         self.bek_label = 'BEK VOLUME'
+        self.bek_filesystem = 'vfat'
+        self.wrong_fs_msg = "BEK does not have vfat filesystem."
+        self.not_mounted_msg = "BEK is not mounted."
+        self.partition_missing_msg = "BEK disk does not expected partition."
+        self.bek_missing_msg = "BEK disk is not attached."
 
     def generate_passphrase(self):
         if TestHooks.use_hard_code_passphrase:
@@ -89,6 +100,9 @@ class BekUtil(object):
                 if bek_filename in filename:
                     return os.path.join(self.bek_filesystem_mount_point, filename)
 
+        except BekMissingException:
+            raise
+
         except Exception as e:
             # use traceback to convert exception to string on both python2 and python3+
             message = "Failed to get BEK from BEK VOLUME with error: {0}".format(traceback.format_exc(e))
@@ -97,7 +111,44 @@ class BekUtil(object):
         return None
 
     def mount_bek_volume(self):
+        bek_expected, fault_reason = self.is_bek_volume_mounted_and_formatted()
+        if bek_expected:
+            self.logger.log("BEK Volume already in expected state.")
+            return
+        else:
+            self.logger.log("Trying to mount BEK volume.")
         self.disk_util.mount_by_label(self.bek_label, self.bek_filesystem_mount_point, "fmask=077")
+        bek_expected, fault_reason = self.is_bek_volume_mounted_and_formatted()
+        if not bek_expected:
+            bek_attached, error_reason = self.is_bek_disk_attached_and_partitioned()
+            if bek_attached:
+                raise BekMissingException("BEK disk missing or not initialized." + fault_reason)
+            else:
+                raise BekMissingException("BEK disk missing or not initialized." + error_reason)
+
+    def is_bek_volume_mounted_and_formatted(self):
+        mount_items=self.disk_util.get_mount_items()
+        for mount_item in mount_items:
+            if os.path.normpath(mount_item["dest"]) == os.path.normpath(self.bek_filesystem_mount_point):
+                if mount_item["fs"] == self.bek_filesystem:
+                    return True, ""
+                else:
+                    self.logger.log("BEK has unexpected filesystem "+mount_item["fs"])
+                    return False, self.wrong_fs_msg
+        return False, self.not_mounted_msg
+
+    def is_bek_disk_attached_and_partitioned(self):
+        possible_bek_locations = [
+            os.path.join(CommonVariables.azure_symlinks_dir, "BEK"),
+            os.path.join(CommonVariables.cloud_symlinks_dir, "scsi0/lun3")
+        ]
+        for location in possible_bek_locations:
+            if os.path.exists(location):
+                if os.path.exists(location+"-part1"):
+                    return True, ""
+                else:
+                    return False, self.partition_missing_msg
+        return False, self.bek_missing_msg
 
     def umount_azure_passhprase(self, encryption_config, force=False):
         passphrase_file = self.get_bek_passphrase_file(encryption_config)
---
+++ VMEncryption/main/test/test_bek_util.py
@@ -0,0 +1,91 @@
+import unittest
+try:
+    import unittest.mock as mock  # python 3+
+except ImportError:
+    import mock  # python2
+
+from BekUtil import BekUtil, BekMissingException
+from DiskUtil import DiskUtil
+from Common import CommonVariables
+from .console_logger import ConsoleLogger
+
+class Test_Bek_Util(unittest.TestCase):
+    def setUp(self):
+        self.logger = ConsoleLogger()
+
+    @mock.patch('DiskUtil.DiskUtil', autospec=True)
+    def test_is_bek_volume_mounted_and_formatted_expected(self, disk_util_mock):
+        bek_util = BekUtil(disk_util_mock, self.logger)
+        disk_util_mock.get_mount_items.return_value = [{"src":"/dev/sdc1", "dest":"/mnt/azure_bek_disk", "fs":"vfat"}]
+        bek_expected, fault_reason = bek_util.is_bek_volume_mounted_and_formatted()
+        self.assertTrue(bek_expected)
+
+    @mock.patch('DiskUtil.DiskUtil', autospec=True)
+    def test_is_bek_volume_mounted_and_formatted_not_mounted(self, disk_util_mock):
+        bek_util = BekUtil(disk_util_mock, self.logger)
+        disk_util_mock.get_mount_items.return_value = [{"src":"/dev/sdc1", "dest":"/nobek", "fs":"vfat"}, {"src":"/dev/sda1", "dest":"/", "fs":"ext4"}]
+        bek_expected, fault_reason = bek_util.is_bek_volume_mounted_and_formatted()
+        self.assertFalse(bek_expected)
+        self.assertEqual(fault_reason, bek_util.not_mounted_msg)
+
+    @mock.patch('DiskUtil.DiskUtil', autospec=True)
+    def test_is_bek_volume_mounted_and_formatted_wrong_fs(self, disk_util_mock):
+        bek_util = BekUtil(disk_util_mock, self.logger)
+        disk_util_mock.get_mount_items.return_value = [{"src":"/dev/sdc1", "dest":"/mnt/azure_bek_disk", "fs":"wrongFS"}, {"src":"/dev/sda1", "dest":"/", "fs":"ext4"}]
+        bek_expected, fault_reason = bek_util.is_bek_volume_mounted_and_formatted()
+        self.assertFalse(bek_expected)
+        self.assertEqual(fault_reason, bek_util.wrong_fs_msg)
+
+    @mock.patch('os.path.exists')
+    @mock.patch('DiskUtil.DiskUtil', autospec=True)
+    def test_is_bek_disk_attached_and_partitioned_expected_gen1(self, disk_util_mock, exists_mock):
+        bek_util = BekUtil(disk_util_mock, self.logger)
+        exists_mock.side_effect = [True, True]
+        bek_attached, error_reason = bek_util.is_bek_disk_attached_and_partitioned()
+        self.assertTrue(bek_attached)
+
+    @mock.patch('os.path.exists')
+    @mock.patch('DiskUtil.DiskUtil', autospec=True)
+    def test_is_bek_disk_attached_and_partitioned_not_attached_gen1(self, disk_util_mock, exists_mock):
+        bek_util = BekUtil(disk_util_mock, self.logger)
+        exists_mock.side_effect = [False, False]
+        bek_attached, error_reason = bek_util.is_bek_disk_attached_and_partitioned()
+        self.assertFalse(bek_attached)
+        self.assertEqual(error_reason, bek_util.bek_missing_msg)
+
+    @mock.patch('os.path.exists')
+    @mock.patch('DiskUtil.DiskUtil', autospec=True)
+    def test_is_bek_disk_attached_and_partitioned_not_partitioned_gen1(self, disk_util_mock, exists_mock):
+        bek_util = BekUtil(disk_util_mock, self.logger)
+        exists_mock.side_effect = [True, False]
+        bek_attached, error_reason = bek_util.is_bek_disk_attached_and_partitioned()
+        self.assertFalse(bek_attached)
+        self.assertEqual(error_reason, bek_util.partition_missing_msg)
+
+    @mock.patch('os.path.exists')
+    @mock.patch('DiskUtil.DiskUtil', autospec=True)
+    def test_is_bek_disk_attached_and_partitioned_expected_gen2(self, disk_util_mock, exists_mock):
+        bek_util = BekUtil(disk_util_mock, self.logger)
+        exists_mock.side_effect = [False, True, True]
+        bek_attached, error_reason = bek_util.is_bek_disk_attached_and_partitioned()
+        self.assertTrue(bek_attached)
+
+    @mock.patch('os.path.exists')
+    @mock.patch('DiskUtil.DiskUtil', autospec=True)
+    def test_is_bek_disk_attached_and_partitioned_not_attached_gen2(self, disk_util_mock, exists_mock):
+        bek_util = BekUtil(disk_util_mock, self.logger)
+        exists_mock.side_effect = [False, False]
+        bek_attached, error_reason = bek_util.is_bek_disk_attached_and_partitioned()
+        self.assertFalse(bek_attached)
+        self.assertEqual(error_reason, bek_util.bek_missing_msg)
+
+    @mock.patch('os.path.exists')
+    @mock.patch('DiskUtil.DiskUtil', autospec=True)
+    def test_is_bek_disk_attached_and_partitioned_not_partitioned_gen2(self, disk_util_mock, exists_mock):
+        bek_util = BekUtil(disk_util_mock, self.logger)
+        exists_mock.side_effect = [False, True, False]
+        bek_attached, error_reason = bek_util.is_bek_disk_attached_and_partitioned()
+        self.assertFalse(bek_attached)
+        self.assertEqual(error_reason, bek_util.partition_missing_msg)
+
+    
