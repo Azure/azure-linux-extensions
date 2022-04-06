4ebdb274c86fcfa9151810211408b0dfc3a2f4d2
aswatt <54380303+aswatt@users.noreply.github.com>
2022-04-05 20:02:20 -0700

    Add Mariner 2.0 support to AMA

--- AzureMonitorAgent/agent.py
+++ AzureMonitorAgent/agent.py
@@ -1194,7 +1194,8 @@ def is_vm_supported_for_extension(operation):
                        'debian' : ['9', '10'], # Debian
                        'ubuntu' : ['16.04', '18.04', '20.04'], # Ubuntu
                        'suse' : ['12'], 'sles' : ['15'], # SLES
-                       'cbl-mariner' : ['1'] # Mariner
+                       'cbl-mariner' : ['1'], # Mariner 1.0
+                       'mariner' : ['2.0'] # Mariner 2.0
     }
 
     vm_supported = False
@@ -1258,7 +1259,7 @@ def get_ssl_cert_info(operation):
         if distro.startswith(name):
             return 'SSL_CERT_DIR', '/etc/ssl/certs'
 
-    for name in ['centos', 'redhat', 'red hat', 'oracle', 'cbl-mariner', "rhel"]:
+    for name in ['centos', 'redhat', 'red hat', 'oracle', 'cbl-mariner', 'mariner', 'rhel']:
         if distro.startswith(name):
             return 'SSL_CERT_FILE', '/etc/pki/tls/certs/ca-bundle.crt'
 
