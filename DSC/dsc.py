c7720bf52c34aea62faa56c294ef9c3918052eb2
adishijha <adishijha@microsoft.com>
2022-05-09 14:39:10 +0530

    add rpm files

--- DSC/dsc.py
+++ DSC/dsc.py
@@ -44,7 +44,7 @@ ExtensionShortName = 'DSCForLinux'
 DownloadDirectory = 'download'
 
 omi_package_prefix = 'packages/omi-1.6.9-1.ssl_'
-dsc_package_prefix = 'packages/dsc-1.1.1-926.ssl_'
+dsc_package_prefix = 'packages/dsc-1.1.1-927.ssl_'
 omi_major_version = 1
 omi_minor_version = 6
 omi_build = 9
@@ -52,7 +52,7 @@ omi_release = 1
 dsc_major_version = 1
 dsc_minor_version = 1
 dsc_build = 1
-dsc_release = 926
+dsc_release = 927
 package_pattern = '(\d+).(\d+).(\d+).(\d+)'
 nodeid_path = '/etc/opt/omi/conf/dsc/agentid'
 date_time_format = "%Y-%m-%dT%H:%M:%SZ"
