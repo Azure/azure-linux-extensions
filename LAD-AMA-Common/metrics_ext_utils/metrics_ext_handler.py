a43fd1359fa83f36d9aa6d1a0d2f1dbaa282ea56
thperapp <thperapp@microsoft.com>
2022-06-06 15:25:40 -0700

    Updated error message

--- LAD-AMA-Common/metrics_ext_utils/metrics_ext_handler.py
+++ LAD-AMA-Common/metrics_ext_utils/metrics_ext_handler.py
@@ -620,7 +620,7 @@ def setup_me(is_lad, HUtilObj=None):
     
     except Exception as e:
         aad_auth_url = "https://login.windows.net/72f988bf-86f1-41af-91ab-2d7cd011db47"
-        message = "Unable to obtain auth url, falling back to use default value: " + aad_auth_url
+        message = "Unable to find AAD Authentication URL in the request error response. Falling back to default value: " + aad_auth_url
         if HUtilObj is not None:
             HUtilObj.log(message)
         else:
