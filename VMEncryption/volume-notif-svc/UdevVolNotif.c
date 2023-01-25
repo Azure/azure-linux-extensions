a386954ad22041a6f8f424ec3e79a82597f00738
Fikret Can <fican@microsoft.com>
2023-01-25 15:21:58 -0800

    Linux VM disk encryption: add a systemd service for detecting arrived/changed volumes

--- VMEncryption/volume-notif-svc/UdevVolNotif.c
+++ VMEncryption/volume-notif-svc/UdevVolNotif.c
@@ -1,23 +1,30 @@
 #include <libudev.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <time.h>
 #include <errno.h>
 #include <signal.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #define LOG_PRIORITY 3
 #define LOG_LEVEL 3
 #define LOG_TO_CONSOLE 1
 
+#define LOG_DIRETORY "/tmp"
+#define LOG_FILE_TEMPLATE "%s/ade_vol_notif-%s.log"
+
 // TODO: Make this a parameter to the program
-static const char *LOG_FILE = "/tmp/ade_vol_notif.log";
+static char log_file_path[1024];
 
 void custom_log(const char *format, ...) {
-    FILE *log_file = fopen(LOG_FILE, "a");
+    FILE *log_file = fopen(log_file_path, "a");
     if (!log_file)
     {
-        fprintf(stderr, "Can't open log file %s", LOG_FILE);
+        fprintf(stderr, "Can't open log file %s", log_file_path);
         exit(1);
     }
 
@@ -45,6 +52,7 @@ void custom_log(const char *format, ...) {
         va_end(args);
     }
 
+    // TODO: Log to syslog additionally.
 }
 
 void sigint_handler(int signum) {
@@ -52,12 +60,74 @@ void sigint_handler(int signum) {
     exit(0);
 }
 
+void prepare_log_file() {
+    // Create log file with date time in UTC.
+    time_t now = time(NULL);
+    struct tm *timeinfo;
+    timeinfo = gmtime(&now);
+    char current_time[80];
+    strftime(current_time, sizeof(current_time), "%Y-%m-%dT%H-%M-%S", timeinfo);
+    snprintf(log_file_path, sizeof(log_file_path), LOG_FILE_TEMPLATE, LOG_DIRETORY, current_time);
+}
+
+void daemonize(int argc, char *argv[])
+{
+    custom_log("Going to daemonize");
+    pid_t pid = fork();
+    if (pid < 0) {
+        custom_log("ERROR: Can't fork (error no=%d)\n", errno);
+        exit(1);
+    }
+    if (pid > 0) {
+        custom_log("Parent process is exiting, pid=%d", pid);
+        exit(0);
+    }
+    umask(0);
+    pid_t sid = setsid();
+    if (sid < 0) {
+        custom_log("ERROR: Can't set sid (error no=%d)\n", errno);
+        exit(1);
+    }
+    if ((chdir("/")) < 0) {
+        custom_log("ERROR: Can't change directory (error no=%d)\n", errno);
+        exit(1);
+    }
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    //execve(argv[0], argv, NULL);
+    // char *newargv[] = { NULL };
+    // execv(argv[0], newargv);
+}
+
 int main(int argc, char *argv[]) {
+
+    prepare_log_file();
 
     custom_log("\n\n### Azure Disk Encryption volume notification service ###");
     custom_log("Starting udev volume notification program (%s) with %d args", argv[0], argc);
     signal(SIGINT, sigint_handler);
     custom_log("Registered SIGINT handler");
+
+    // Command line options: -d for daemon mode
+    int c = 0;
+    int daemon_mode = 0;
+    while ((c = getopt(argc, argv, "d")) != -1) {
+        switch (c) {
+            case 'd':
+                custom_log("Option for switching to daemon mode provided");
+                daemon_mode = 1;
+                break;
+            default:
+                custom_log("Unknown option %c", c);
+                exit(1);
+        }
+    }
+
+    if (daemon_mode) {
+        daemonize(argc, argv);
+    }
 
     struct udev *udev;
     struct udev_monitor *mon;
