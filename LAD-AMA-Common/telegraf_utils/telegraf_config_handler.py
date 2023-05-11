6929db1d71f4d8d819ddfa2a91e512128e9ae98d
Siddhartha Mathiharan <42589365+simathih@users.noreply.github.com>
2023-05-11 10:25:56 -0700

    Handle Linux Hardening where sudo on root is blocked

--- LAD-AMA-Common/telegraf_utils/telegraf_config_handler.py
+++ LAD-AMA-Common/telegraf_utils/telegraf_config_handler.py
@@ -512,6 +512,8 @@ def stop_telegraf_service(is_lad):
 
         if os.path.isfile(telegraf_service_path):
             code = os.system("sudo systemctl stop {0}".format(telegraf_service_name))
+            if code != 0:
+                code = os.system("systemctl stop {0}".format(telegraf_service_name))                
         else:
             return False, "Telegraf service file does not exist. Failed to stop telegraf service: {0}.service.".format(telegraf_service_name)
 
@@ -589,11 +591,13 @@ def setup_telegraf_service(is_lad, telegraf_bin, telegraf_d_conf_dir, telegraf_a
 
             daemon_reload_status = os.system("sudo systemctl daemon-reload")
             if daemon_reload_status != 0:
-                message = "Unable to reload systemd after Telegraf service file change. Failed to setup telegraf service. Exit code:" + str(daemon_reload_status)
-                if HUtilObj is not None:
-                    HUtilObj.log(message)
-                else:
-                    print('Info: {0}'.format(message))
+                daemon_reload_status = os.system("systemctl daemon-reload")
+                if daemon_reload_status != 0:
+                    message = "Unable to reload systemd after Telegraf service file change. Failed to setup telegraf service. Exit code:" + str(daemon_reload_status)
+                    if HUtilObj is not None:
+                        HUtilObj.log(message)
+                    else:
+                        print('Info: {0}'.format(message))
 
         else:
             raise Exception("Unable to copy Telegraf service template file to {0}. Failed to setup telegraf service.".format(telegraf_service_path))
@@ -631,8 +635,10 @@ def start_telegraf(is_lad):
     if metrics_utils.is_systemd():
         service_restart_status = os.system("sudo systemctl restart {0}".format(telegraf_service_name))
         if service_restart_status != 0:
-            log_messages += "Unable to start Telegraf service. Failed to start telegraf service."
-            return False, log_messages
+            service_restart_status = os.system("systemctl restart {0}".format(telegraf_service_name))
+            if service_restart_status != 0:
+                log_messages += "Unable to start Telegraf service. Failed to start telegraf service."
+                return False, log_messages
 
     # Otherwise, start telegraf as a process and save the pid to a file so that we can terminate it while disabling/uninstalling
     else:
