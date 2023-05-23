2d71e0b349799c1b5bf59a14111ded9e007fdb72
Siddhartha Mathiharan <42589365+simathih@users.noreply.github.com>
2023-05-23 07:37:05 -0700

    Update Syslog TCP feature flag name (#1720)
    * Update Syslog TCP feature flag name
    
    * Update agent.py
    
    * Update agent.py

--- AzureMonitorAgent/agent.py
+++ AzureMonitorAgent/agent.py
@@ -1201,8 +1201,14 @@ def generate_localsyslog_configs():
         f = open(AMASyslogPortFilePath, "r")
         syslog_port = f.read()
         f.close()
-
-    if public_settings is not None and "useOmfwd" in public_settings and syslog_port != '':
+        
+    useSyslogTcp = False
+    if public_settings is not None and "features" in public_settings:
+        features = public_settings.get("features")
+        if features is not None and "useSyslogTcp" in features:            
+            useSyslogTcp = features.get("useSyslogTcp")    
+    
+    if useSyslogTcp == True and syslog_port != '':
         if os.path.exists('/etc/rsyslog.d/'):            
             restartRequired = False
             if not os.path.exists('/etc/rsyslog.d/10-azuremonitoragent-omfwd.conf'):
