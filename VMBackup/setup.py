aaa206048c634be15135515cea2c94cf2cbed05e
arisettisanjana <varshasanjana29@gmail.com>
2023-08-25 21:54:49 +0530

    added a binary

--- VMBackup/setup.py
+++ VMBackup/setup.py
@@ -116,13 +116,14 @@ while(process_wait_time >0 and chil.poll() is None):
 
 os.chdir(cur_dir)
 
+
 '''
-commenting out the steps for generating binaries for safefreezeARM64
 due to the lack of cross-compilation support in Mariner.
-It would not be affecting our code as we are using the older binaries
+It would not be able to get the binaries, so will be using the older binaries
 To Do : Once Mariner starts supporting cross-compilation, 
-will have to uncomment the steps and modify any necessary scripts to
+will have to modify any necessary scripts to
 enable the generation of safefreezeARM64 binaries.
+'''
 """
 generate the ARM64 safe freeze binary
 """
@@ -136,7 +137,7 @@ while(process_wait_time >0 and chil.poll() is None):
 
 os.chdir(cur_dir)
 
-'''
+
 
 """
 setup script, to package the files up
