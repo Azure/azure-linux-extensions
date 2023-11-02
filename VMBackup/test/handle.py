5c812da73183d44eb857c44778c9a464dc27c7ae
arisettisanjana <varshasanjana29@gmail.com>
2023-11-02 14:56:08 +0530

    modified the file name

--- VMBackup/test/handle.py
+++ VMBackup/test/handle.py
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/env python
+#!/usr/bin/env python
 #
 #CustomScript extension
 #
@@ -27,10 +27,30 @@ import sys
 import imp
 import shlex
 import traceback
-import urllib2
-import urlparse
+#import urllib2
+#import urlparse
 import datetime
 import math
+current = os.path.dirname(os.path.realpath(__file__))
+
+# Getting the parent directory name
+# where the current directory is present.
+parent = os.path.dirname(current)
+ 
+# adding the parent directory to 
+# the sys.path.
+sys.path.append(parent)
+#print(parent)
+sys.path.append('..')
+main = os.path.join(parent,'main')
+#print(main)
+sys.path.append(main)
+from main.Utils import HandlerUtil 
+from patch import GetMyPatching
+from backuplogger import Backuplogger
+from common import CommonVariables
+from Utils import SizeCalculation
+from mock import MagicMock
 
 
 def main():
@@ -44,5 +64,26 @@ def main():
     total_span_in_seconds = timespan.days * 24 * 60 * 60 + timespan.seconds
     print(str(total_span_in_seconds))
 
+    # run a test for exclude disk scenario
+    excludeDisk()
+
+def excludeDisk():
+    Log = MagicMock()
+    Error = MagicMock()
+    hutil = HandlerUtil.HandlerUtility(Log, Error, CommonVariables.extension_name)
+    backup_logger = Backuplogger(hutil)
+    MyPatching, patch_class_name, orig_distro = GetMyPatching(backup_logger)
+    hutil.patching = MyPatching
+    para_parser = MagicMock()
+    para_parser.includedDisks = {"dataDiskLunList":[-1],"isAnyDirectDriveDiskIncluded":None,"isAnyDiskExcluded":True,"isAnyWADiskIncluded":None,"isOSDiskIncluded":False,"isVmgsBlobIncluded":None}
+    para_parser.includeLunList = para_parser.includedDisks["dataDiskLunList"]
+    sizeCalculation = SizeCalculation.SizeCalculation(patching = MyPatching , hutil = hutil, logger = backup_logger, para_parser = para_parser)
+    total_used_size,size_calculation_failed = sizeCalculation.get_total_used_size()
+    print(total_used_size)
+    print(size_calculation_failed)
+
+
+
 if __name__ == '__main__' :
     main()
+
