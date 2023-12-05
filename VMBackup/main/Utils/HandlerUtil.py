2634d6b4b073da2ec984d6108339a0663a16a541
Ankitha Pilli <ankitha7698@gmail.com>
2023-12-05 23:29:16 +0530

    Updated HandlerUtil.py

--- VMBackup/main/Utils/HandlerUtil.py
+++ VMBackup/main/Utils/HandlerUtil.py
@@ -300,8 +300,9 @@ class HandlerUtility:
                     self._context._event_dir = handler_env['handlerEnvironment']['eventsFolder']
                     self.event_dir = self._context._event_dir
             except Exception as e:
-                errorMsg = "The 'eventsFolder' field is missing in handlerEnvironment.json file. Hence skipping event logging! %s" % (str(e), traceback.format_exc())
+                errorMsg = "The 'eventsFolder' field is missing in handlerEnvironment.json file. Hence skipping event logging!"
                 self.log(errorMsg, 'Error')
+                self.log(repr(e), 'Error')
             self._context._status_dir = handler_env['handlerEnvironment']['statusFolder']
             self._context._heartbeat_file = handler_env['handlerEnvironment']['heartbeatFile']
             if seqNo != -1:
