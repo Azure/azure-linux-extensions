649d6e6816c182ee0610c61bba91f2d14270d29c
Bam <braydenmuir@microsoft.com>
2023-12-20 13:30:56 -0800

    Bamuir/repo cleanup (#1859)
    * Remove all but Common, Utils, VM ENcryption
    
    * 1.4.0.6 release

--- Diagnostic/run_unittests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for test in watchertests test_commonActions test_lad_logging_config test_lad_config_all test_LadDiagnosticUtil \
-                test_builtin test_lad_ext_settings; do
-    python -m tests.$test
-done
