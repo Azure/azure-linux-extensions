b22c688945ecc89d60abdea10040d3649c793a23
Kara <33235324+kro-cat@users.noreply.github.com>
2024-08-15 14:25:37 -0500

    fix(distroutils): encode cmd_input argument for ext_utils.run_send_stdin()
    The method ext_utils.run_send_stdin() (in extensionutils.py) requires
    the second argument, cmd_input, to be a bytes object, not a string
    object. This is due to the usage of: subprocess.Popen().communicate().
    Encode the password as utf-8 when passing to ext_utils.run_send_stdin().

--- Utils/distroutils.py
+++ Utils/distroutils.py
@@ -281,7 +281,7 @@ class FreeBSDDistro(GenericDistro):
 
     # noinspection PyMethodOverriding
     def chpasswd(self, user, password, **kwargs):
-        return ext_utils.run_send_stdin(['pw', 'usermod', 'user', '-h', '0'], password, log_cmd=False)
+        return ext_utils.run_send_stdin(['pw', 'usermod', 'user', '-h', '0'], password.encode('utf-8'), log_cmd=False)
 
     def create_account(self, user, password, expiration, thumbprint, enable_nopasswd):
         """
