8c0c89fc25e4ca53561ef0f47d6b507bef9d2ba7
Siddhartha Mathiharan <42589365+simathih@users.noreply.github.com>
2024-08-28 18:21:59 -0700

    Handle unicode output from running commands in python wrapper
    fix for https://portal.microsofticm.com/imp/v5/incidents/details/536301475/summary

--- AzureMonitorAgent/agent.py
+++ AzureMonitorAgent/agent.py
@@ -2229,7 +2229,7 @@ def run_get_output(cmd, chk_err = False, log_cmd = True):
             exit_code = e.returncode
             output = e.output
 
-    if type(output) == str:
+    if type(output) == unicode:
         output = output.encode('utf-8')
 
     # On python 3, encode returns a byte object, so we must decode back to a string
