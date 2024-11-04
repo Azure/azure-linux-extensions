4e03f912eb277f637a39e3df84cbb18ec351405d
Ankitha Pilli <anpilli@microsoft.com>
2024-11-04 13:24:48 +0530

    Add Script to install python 2 version on the VM

--- VMBackup/main/NodeBased/NodeBasedConstants.py
+++ VMBackup/main/NodeBased/NodeBasedConstants.py
@@ -6,9 +6,9 @@ class NodeBasedConstants:
 
     HOST_IP_ADDRESS = "168.63.129.16"
 
-    GET_SNAPSHOT_REQUESTS_URI = "http://{}/xdisksvc/snapshotrequest".format(HOST_IP_ADDRESS)
-    START_SNAPSHOT_REQUESTS_URI = "http://{}/xdisksvc/startsnapshots".format(HOST_IP_ADDRESS)
-    END_SNAPSHOT_REQUESTS_URI = "http://{}/xdisksvc/endsnapshots".format(HOST_IP_ADDRESS)
+    GET_SNAPSHOT_REQUESTS_URI = "http://{0}/xdisksvc/snapshotrequest".format(HOST_IP_ADDRESS)
+    START_SNAPSHOT_REQUESTS_URI = "http://{0}/xdisksvc/startsnapshots".format(HOST_IP_ADDRESS)
+    END_SNAPSHOT_REQUESTS_URI = "http://{0}/xdisksvc/endsnapshots".format(HOST_IP_ADDRESS)
 
     SERVICE_POLLING_INTERVAL_IN_SECS = 300
     EXTENSION_TIMEOUT_IN_MINS = 10
